de2feaaed1a6fb8222adb85cb0c8c5e4ab999b5b
barwar18 <66175869+barwar18@users.noreply.github.com>
2024-04-01 11:15:22 -0400

    Testing database operation
    Functional: Testing basic database GET requests for use in drawing paths on the map.

--- apps/frontend/src/components/mapCanvas.tsx
+++ apps/frontend/src/components/mapCanvas.tsx
@@ -1,5 +1,5 @@
 import { useRef, useEffect } from 'react';
-//import ll1 from "../assets/BWHospitalMaps/00_thelowerlevel1.png";
+import axios from "axios";
 
 type mapCanvasProps = {
     image
@@ -7,6 +7,20 @@ type mapCanvasProps = {
 
 export function MapCanvas(props: mapCanvasProps) {
     const canvasRef = useRef(null);
+    let xLoc: number; // Test x-coordinate for learning to use database
+    axios.get("/api/map").then((res) => {
+        xLoc = res.data.nodes[17].xcoord; // after data. format is array[desiredIndex].property
+    });
+
+    // Create a line on the page at the specified position
+    function drawLine(startX: number, startY: number, endX: number, endY: number) {
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        context.moveTo(startX, startY);
+        context.lineTo(endX, endY);
+        context.stroke();
+    }
 
     useEffect(() => {
         const image = new Image();
@@ -15,14 +29,15 @@ export function MapCanvas(props: mapCanvasProps) {
         const context = canvas.getContext('2d');
 
         image.onload = () => {
-            context.drawImage(image, 0, 0, 200, 100);
+            context.drawImage(image, 0, 0, 5000, 3400); // Change parameters to zoom in and pan around the image
+            drawLine(xLoc, 1070, 2770, 1270);
         };
     } );
 
 
     return (
         <>
-        <canvas ref={canvasRef} width={200} height={100} />
+        <canvas ref={canvasRef} width={5000} height={3400} />
         </>
     );
 }
