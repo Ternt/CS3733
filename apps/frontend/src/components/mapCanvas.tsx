01b9258987cc9219ba38b8cac2dcdc337f7da0b6
barwar18 <66175869+barwar18@users.noreply.github.com>
2024-04-01 20:30:06 -0400

    Point placement functional
    Locations on the map can be selected and a point will be placed on an edge nearest that selection (assuming the selection was close enough to an edge).

--- apps/frontend/src/components/mapCanvas.tsx
+++ apps/frontend/src/components/mapCanvas.tsx
@@ -7,6 +7,94 @@ type mapCanvasProps = {
 
 export function MapCanvas(props: mapCanvasProps) {
   const canvasRef = useRef(null);
+  let nodes, edges;
+  updateMapPaths();
+  console.log(nodes + edges);
+
+  async function updateMapPaths() {
+      await axios.get("/api/map").then((res) => {
+          nodes = res.data.nodes;
+          edges = res.data.edges;
+      });
+  }
+
+    const MAX_SNAP_DIST = 75;
+    type vec2 = {
+        x: number;
+        y: number;
+    };
+    type node = {
+        point: vec2;
+        id: string;
+    };
+    type edge = {
+        startNodeID: node;
+        endNodeID: node;
+    };
+    type findClosestPointOnGraphProps = {
+        x: number;
+        y: number;
+        nodes: node[];
+        edges: edge[];
+    };
+
+    function dot(ax:number, ay:number, bx:number, by:number):number{
+        return ax * bx + ay * by;
+    }
+
+    function clamp(val: number, min: number, max: number):number {
+        return Math.min(Math.max(val, min), max);
+    }
+
+    function distance(a: vec2, b: vec2):number{
+        return Math.sqrt((a.x - b.x) ** 2 + (a.y - b.y) ** 2);
+    }
+
+    function graphHelper(props: findClosestPointOnGraphProps){
+        let closestPoint: vec2 | null = null;
+        let closestDist: number = Infinity;
+
+        for(let i = 0; i < props.edges.length; i++) {
+            const edge = props.edges[i];
+            const startNode = edge.startNodeID;
+            const endNode = edge.endNodeID;
+            if (!startNode || !endNode) continue;
+
+            const pa: vec2 = {
+                x: nodes.filter((node) => node.nodeID === startNode)[0].xcoord,
+                y: nodes.filter((node) => node.nodeID === startNode)[0].ycoord
+            };
+            const pb: vec2 = {
+                x: nodes.filter((node) => node.nodeID === endNode)[0].xcoord,
+                y: nodes.filter((node) => node.nodeID === endNode)[0].ycoord
+            };
+
+            // TODO adjust pa and pb for screen scaling of the image
+            // let x1 = startNode.x * zoomScale + offset.x;
+            // let y1 = startNode.y * zoomScale + offset.y;
+            // let x2 = endNode.x * zoomScale + offset.x;
+            // let y2 = endNode.y * zoomScale + offset.y;
+
+            const t:number =
+                clamp(
+                    dot(props.x - pa.x, props.y - pa.y, pb.x - pa.x, pb.y - pa.y) /
+                    dot(pb.x - pa.x, pb.y - pa.y, pb.x - pa.x, pb.y - pa.y),
+                    0, 1);
+
+            const d:vec2 = { x: pa.x + t * (pb.x - pa.x), y: pa.y + t * (pb.y - pa.y) };
+            const mousePos: vec2 = {x: props.x, y: props.y};
+            const dist:number = distance(d, mousePos);
+            if (dist < closestDist && dist <= MAX_SNAP_DIST){
+                closestDist = dist;
+                closestPoint = d;
+            }
+
+
+        }
+
+        //console.log(closestPoint);
+        return closestPoint;
+    }
 
   /**
    * Draw a path between several nodes
@@ -68,7 +156,26 @@ export function MapCanvas(props: mapCanvasProps) {
     image.src = props.image;
     const canvas = canvasRef.current;
     const context = canvas.getContext("2d");
+      canvas.addEventListener('click', function(e) {
 
+          const rect = canvas.getBoundingClientRect();  // get element's abs. position
+          const x = (e.clientX-rect.left)/(rect.right - rect.left) * canvas.width;
+          const y = (e.clientY - rect.top)/(rect.bottom - rect.top) * canvas.height;
+
+          // Move point to nearest edge
+          try {
+              const coords = graphHelper({x, y, nodes, edges});
+              // Draw circle
+              context.beginPath();
+              context.lineWidth = 15;
+              context.arc(coords.x, coords.y, 5, 0, 2 * Math.PI);
+              context.stroke();
+          } catch (TypeError) {
+            console.log("Selected point too far from any path");
+          }
+
+
+      });
     image.onload = () => {
       context.drawImage(image, 0, 0, 5000, 3400); // Change parameters to zoom in and pan around the image
       createPath(["CHALL004L1", "CLABS005L1", "CREST002L1"]); // Hardcoded path for now.
@@ -83,3 +190,5 @@ export function MapCanvas(props: mapCanvasProps) {
 }
 
 export default MapCanvas;
+
+// /api/a-star-api/startNode=c7utht?endNode=C7jgfg
