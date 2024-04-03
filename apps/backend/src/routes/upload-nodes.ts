218f5a69e09bbf88b27c565234262b8d80515eb2
Alex <alexander.siracusa@gmail.com>
2024-04-03 19:51:54 -0400

    can upload node table

--- apps/backend/src/routes/upload-nodes.ts
+++ apps/backend/src/routes/upload-nodes.ts
@@ -1,12 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import express, { Router, Request, Response } from "express";
 //import { Prisma } from "database";
 //import PrismaClient from "../bin/database-connection.ts";
+import { PrismaClient } from "database";
 
 const router: Router = express.Router();
 
-// function splitLines(t: string): string[] {
-//   return t.split(/\r\n|\r|\n/);
-// }
+function splitLines(t: string): string[] {
+  return t.split(/\r\n|\r|\n/);
+}
 
 router.post("/", async function (req: Request, res: Response) {
   if (!req.files || Object.keys(req.files).length === 0) {
@@ -14,8 +18,58 @@ router.post("/", async function (req: Request, res: Response) {
     return;
   }
 
+  // Get file data
   const files = req.files;
-  console.log(files.data.toString());
+  const str: string = files.file.data.toString();
+  // ^ eslint does not like this line
+
+  // Check if headers are included in the file
+  let header: boolean = true;
+  if (req.query.header !== undefined) {
+    if (req.query.header!.toString() === "true") {
+      header = true;
+    } else if (req.query.header!.toString() === "false") {
+      header = false;
+    } else {
+      console.log("header must be 'true' or 'false'");
+      res.status(200);
+      return;
+    }
+  }
+
+  const prisma = new PrismaClient();
+
+  try {
+    // parse csv
+    const lines = splitLines(str).filter((line) => /\S/.test(line));
+    if (header) {
+      lines.shift();
+    }
+    const rows = lines.map((line) => line.split(","));
+
+    const data = Array.from(
+      rows.map((row) => ({
+        nodeID: row[0],
+        xcoord: parseInt(row[1]),
+        ycoord: parseInt(row[2]),
+        floor: row[3],
+        building: row[4],
+        nodeType: row[5],
+        longName: row[6],
+        shortName: row[7],
+      })),
+    );
+
+    // upload node file
+    await prisma.$transaction([
+      prisma.nodeDB.deleteMany(),
+      prisma.nodeDB.createMany({ data }),
+    ]);
+  } catch {
+    console.log("node file upload failed");
+    res.sendStatus(400);
+    return;
+  }
 
   res.sendStatus(200);
 });
