789fd3a7101d39084cf9248f2b816c601897227f
Abysssigher <80065903+Abysssigher@users.noreply.github.com>
2024-04-03 08:42:56 -0400

    display map data in a table

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,44 +1,49 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 import ServiceRequest from "./serviceRequest.tsx";
-import { ThemeProvider } from "@mui/material/styles"; // Import ThemeProvider
+import EdgeCSVDisplay from "./pages/TableDisplayPage/displayCSV.tsx";
+import {ThemeProvider} from "@mui/material/styles"; // Import ThemeProvider
 import CustomTheme from "./components/CustomTheme.tsx"; // Import your custom theme
 import MapPage from "./routes/mapPage.tsx";
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      errorElement: <div />,
-      element: <Root />,
-      children: [
+    const router = createBrowserRouter([
         {
-          path: "",
-          element: <ServiceRequest />,
+            path: "/",
+            errorElement: <div/>,
+            element: <Root/>,
+            children: [
+                {
+                    path: "",
+                    element: <ServiceRequest/>,
+                },
+                {
+                    path: "/map",
+                    element: <MapPage/>,
+                },
+                {
+                    path: "/table",
+                    element: <EdgeCSVDisplay/>,
+                },
+            ],
         },
-        {
-          path: "/map",
-          element: <MapPage />,
-        },
-      ],
-    },
-  ]);
+    ]);
 
-  return (
-    <ThemeProvider theme={CustomTheme}>
-      {" "}
-      {/* Wrap RouterProvider with ThemeProvider */}
-      <RouterProvider router={router} />
-    </ThemeProvider>
-  );
-        
-  function Root() {
     return (
-      <div className="w-full flex flex-col px-20 gap-5">
-        <Outlet />
-      </div>
+        <ThemeProvider theme={CustomTheme}>
+            {" "}
+            {/* Wrap RouterProvider with ThemeProvider */}
+            <RouterProvider router={router}/>
+        </ThemeProvider>
     );
-  }
+
+    function Root() {
+        return (
+            <div className="w-full flex flex-col px-20 gap-5">
+                <Outlet/>
+            </div>
+        );
+    }
 }
 
 export default App;
