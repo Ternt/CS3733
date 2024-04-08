937ccf683aa2e7557ae046d0645457c7e34d7490
Abysssigher <80065903+Abysssigher@users.noreply.github.com>
2024-04-08 16:34:46 -0400

    format the sanitation request page (still need to fix background picture)

--- apps/frontend/src/components/Sanitation Request/SanitaitonRequestForm.tsx
+++ apps/frontend/src/components/Sanitation Request/SanitaitonRequestForm.tsx
@@ -1,126 +1,266 @@
-import { ChangeEvent, useState } from "react";
-import { SanitationFormFields } from "./sanitationFields.ts";
+import {ChangeEvent, useState} from "react";
+import {SanitationFormFields} from "./sanitationFields.ts";
 import RequestList from "../../helpers/requestList.ts";
-import Dropdown from "../Form Elements/Dropdown.tsx";
+// import Dropdown from "../Form Elements/Dropdown.tsx";
 import Checkbox from "../Form Elements/Checkbox.tsx";
 import Radio from "../Form Elements/Radio.tsx";
+// import "./SanitationRequestForm.scss";
+import sanitationImage from './sanitation_background.jpg';
+import {
+    Grid,
+    TextField,
+    FormControl,
+    InputLabel,
+    Select,
+    MenuItem,
+    Button,
+    Box,
+    Typography, TableContainer, Table, TableHead, TableRow, TableCell, TableBody
+    // RadioGroup,
+    // FormControlLabel,
+    // FormGroup,
+    // Checkbox,
+    // Radio
+
+} from '@mui/material';
 
 const sanitationRequests = new RequestList();
 
 function SanitationRequestForm() {
-  const [formInput, setFormInput] = useState<SanitationFormFields>({
-    name: "",
-    priority: "Low",
-    location: "",
-    type: "Unspecified",
-    size: "Unspecified",
-    assignmentStatus: "Unassigned",
-  });
+    const [submittedRequests, setSubmittedRequests] = useState([]);
 
-  function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
-    setFormInput({ ...formInput, name: e.target.value });
-  }
-
-  function handleLocationInput(e: ChangeEvent<HTMLInputElement>) {
-    setFormInput({ ...formInput, location: e.target.value });
-  }
-
-  function handleTypeInput() {
-    // @ts-expect-error Specified type more specifically and typescript doesn't like that
-    const boxes: NodeListOf<HTMLInputElement> =
-      document.getElementsByName("type");
-    let checkedBoxes = "";
-    for (let i = 0; i < boxes.length; i++) {
-      if (boxes[i].checked) {
-        checkedBoxes += boxes[i].value + " ";
-      }
-    }
-    setFormInput({ ...formInput, type: checkedBoxes });
-  }
-
-  function updateSizeInput(input: string) {
-    setFormInput({ ...formInput, size: input });
-  }
-
-  function submitForm() {
-    sanitationRequests.addRequestToList(formInput);
-    clearForm();
-    console.log(sanitationRequests.requests); // Print the array of requests to the console
-  }
-
-  function clearForm() {
-    setFormInput({
-      ...formInput,
-      name: "",
-      priority: "Low",
-      location: "",
-      type: "Unspecified",
-      size: "Unspecified",
-      assignmentStatus: "Unassigned",
+    const [formInput, setFormInput] = useState<SanitationFormFields>({
+        name: "",
+        priority: "Low",
+        location: "",
+        type: "Unspecified",
+        size: "Unspecified",
+        assignmentStatus: "Unassigned",
     });
-    const form = document.getElementById("sanitationForm");
-    if (form == null) {
-      return;
-    } else {
-      // @ts-expect-error Declaring form as an HTMLFormElement causes errors with getting
-      form.reset();
+
+    function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
+        setFormInput({...formInput, name: e.target.value});
     }
-  }
 
-  return (
-    <>
-      <div className="santitationRequestDiv">
-        <h2>Sanitation Service Request Form</h2>
-        <form id="sanitationForm">
-          <label>Your Name: </label>
-          <input onChange={handleNameInput} value={formInput.name} />
+    function handleLocationInput(e: ChangeEvent<HTMLInputElement>) {
+        setFormInput({...formInput, location: e.target.value});
+    }
 
-          <Dropdown
-            label={"Priority:"}
-            onChange={(v: string) => {
-              setFormInput({ ...formInput, assignmentStatus: v });
-            }}
-            items={["Low", "Medium", "High", "Emergency"]}
-            defaultValue={"Low"}
-          />
+    function handleTypeInput() {
+        // @ts-expect-error Specified type more specifically and typescript doesn't like that
+        const boxes: NodeListOf<HTMLInputElement> =
+            document.getElementsByName("type");
+        let checkedBoxes = "";
+        for (let i = 0; i < boxes.length; i++) {
+            if (boxes[i].checked) {
+                checkedBoxes += boxes[i].value + ", ";
+            }
+        }
+        setFormInput({...formInput, type: checkedBoxes});
+    }
 
-          <label>Location: </label>
-          <input onChange={handleLocationInput} value={formInput.location} />
+    function updateSizeInput(input: string) {
+        setFormInput({...formInput, size: input});
+    }
 
-          <Checkbox
-            label={"Type of mess (Choose all that apply):"}
-            onChange={handleTypeInput}
-            groupName={"type"}
-            items={["Solid Waste", "Liquid Spill", "Other"]}
-          />
+    function submitForm() {
+        setSubmittedRequests(prevRequests => [...prevRequests, formInput]);
+        sanitationRequests.addRequestToList(formInput);
+        clearForm();
+        console.log(sanitationRequests.requests); // Print the array of requests to the console
+    }
 
-          <Radio
-            label={"Mess Size"}
-            onChange={updateSizeInput}
-            groupName={"messSize"}
-            items={["Small", "Medium", "Large"]}
-          />
+    function clearForm() {
+        setFormInput({
+            ...formInput,
+            name: "",
+            priority: "Low",
+            location: "",
+            type: "Unspecified",
+            size: "Unspecified",
+            assignmentStatus: "Unassigned",
+        });
+        const form = document.getElementById("sanitationForm");
+        if (form == null) {
+            return;
+        } else {
+            // @ts-expect-error Declaring form as an HTMLFormElement causes errors with getting
+            form.reset();
+        }
+    }
 
-          <Dropdown
-            label={"Assignment Status:"}
-            onChange={(v: string) => {
-              setFormInput({ ...formInput, assignmentStatus: v });
-            }}
-            items={["Unassigned", "Assigned", "In Progress", "Closed"]}
-            defaultValue={"Unassigned"}
-          />
-          <button type="button" onClick={submitForm}>
-            Submit Request
-          </button>
-          <br />
-          <br />
-          <button type="button" onClick={clearForm}>
-            Clear
-          </button>
-        </form>
-      </div>
-    </>
-  );
+    return (
+        <Box position="relative"
+             sx={{
+                 backgroundImage: `url(${sanitationImage})`,
+                 backgroundColor: '#5f6f8a',
+                 display: 'flex',
+                 justifyContent: 'center',
+                 alignItems: 'center',
+                 height: '100vh',
+                 backgroundSize: 'cover'
+             }}
+        >
+            <Box position="absolute"
+                 top={70}
+                 sx={{
+                     width: '500px',
+                     display: 'flex',
+                     justifyContent: 'center',
+                     backgroundColor: '#012d5a',
+                     color: '#f6bd38',
+                     p: 2
+                 }}
+            >
+                <Typography variant="h4" component="h1" align="center">
+                    Sanitation Request
+                </Typography>
+            </Box>
+
+            <Box position="absolute"
+                 top={150}
+                 sx={{width: '500px'}}>
+                <form
+                    id="sanitationForm"
+                    style={{
+                        backgroundColor: 'whitesmoke',
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                    <Grid container spacing={2} p={10} pt={2}>
+
+                        <Grid item xs={12}>
+                            <InputLabel>Employee Name</InputLabel>
+                            <TextField
+                                label="Your Name"
+                                onChange={handleNameInput}
+                                value={formInput.name}
+                                fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <InputLabel>Priority</InputLabel>
+                            <FormControl fullWidth>
+                                <Select
+                                    value={formInput.priority}
+                                    onChange={(event) => {
+                                        setFormInput({...formInput, priority: event.target.value});
+                                    }}
+                                >
+                                    <MenuItem value={"Low"}>Low</MenuItem>
+                                    <MenuItem value={"Medium"}>Medium</MenuItem>
+                                    <MenuItem value={"High"}>High</MenuItem>
+                                    <MenuItem value={"Emergency"}>Emergency</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <InputLabel>Location of Mess</InputLabel>
+                            <TextField
+                                label="Location"
+                                onChange={handleLocationInput}
+                                value={formInput.location}
+                                fullWidth
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Checkbox
+                                label={"Type of mess (Choose all that apply):"}
+                                onChange={handleTypeInput}
+                                groupName={"type"}
+                                items={["Solid Waste", "Liquid Spill", "Other"]}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Radio
+                                label={"Mess Size"}
+                                onChange={updateSizeInput}
+                                groupName={"messSize"}
+                                items={["Small", "Medium", "Large"]}
+                            />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <InputLabel>Assignment Status:</InputLabel>
+                            <FormControl fullWidth>
+                                <Select
+                                    value={formInput.assignmentStatus}
+                                    onChange={(event) => {
+                                        setFormInput({...formInput, assignmentStatus: event.target.value});
+                                    }}
+                                >
+                                    <MenuItem value={"Unassigned"}>Unassigned</MenuItem>
+                                    <MenuItem value={"Assigned"}>Assigned</MenuItem>
+                                    <MenuItem value={"In Progress"}>In Progress</MenuItem>
+                                    <MenuItem value={"Closed"}>Closed</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Grid>
+
+                        <Grid item xs={12}
+                              style={{display: 'flex', justifyContent: 'space-between'}}>
+                            <Button variant="contained" onClick={submitForm}>
+                                Submit Request
+                            </Button>
+                            <Button variant="outlined" onClick={clearForm}>
+                                Clear
+                            </Button>
+                        </Grid>
+
+                    </Grid>
+                </form>
+            </Box>
+
+            <Box position="absolute"
+                 top={1000}
+                sx={{ width: '80%'}}>
+                <TableContainer>
+                    <Table>
+                        <TableHead sx={{
+                            backgroundColor: '#012d5a'
+                        }}>
+                            <TableRow>
+                                <TableCell sx={{ color: '#f6bd38' }}>Name</TableCell>
+                                <TableCell sx={{ color: '#f6bd38' }}>Priority</TableCell>
+                                <TableCell sx={{ color: '#f6bd38' }}>Location</TableCell>
+                                <TableCell sx={{ color: '#f6bd38' }}>Type</TableCell>
+                                <TableCell sx={{ color: '#f6bd38' }}>Size</TableCell>
+                                <TableCell sx={{ color: '#f6bd38' }}>Assignment Status</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {submittedRequests.map((request, index) => (
+                                <TableRow key={index}>
+                                    <TableCell component="th" scope="row">
+                                        {request.name}
+                                    </TableCell>
+                                    <TableCell>{request.priority}</TableCell>
+                                    <TableCell>{request.location}</TableCell>
+                                    <TableCell>{request.type}</TableCell>
+                                    <TableCell>{request.size}</TableCell>
+                                    <TableCell>{request.assignmentStatus}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Box>
+
+
+
+
+
+
+
+        </Box>
+
+)
+    ;
 }
 
 export default SanitationRequestForm;
+
