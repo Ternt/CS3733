0a2e5941cc339b3a32f2950dc3d588e0d3c910c6
Alex <alexander.siracusa@gmail.com>
2024-04-08 15:23:08 -0400

    added map node files and route to gift request page

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -9,6 +9,7 @@ import ServiceRequests from "./showAllSR.tsx";
 import MapDataDisplay from "./pages/TableDisplayPage/displayCSV.tsx";
 import TouchToStart from "./components/TouchToStart/TouchToStart.tsx";
 import NavBar from "./components/navbar/navbar.tsx";
+import GiftRequestPage from "./pages/GiftRequestPage/GiftRequestPage.tsx";
 
 function App() {
   const router = createBrowserRouter([
@@ -32,6 +33,10 @@ function App() {
         {
           path: "/service-request-display",
           element: <ServiceRequests />,
+        },
+        {
+          path: "/gift-request",
+          element: <GiftRequestPage />,
         },
         {
           path: "/tables",
