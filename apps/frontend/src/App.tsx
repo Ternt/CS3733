98d4e7205c3abd2ad34dd79fe99bb93a89e30ca2
Alex <alexander.siracusa@gmail.com>
2024-04-09 13:42:53 -0400

    fixed sanitation request

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -13,7 +13,7 @@ import GiftRequestPage from "./pages/GiftRequestPage/GiftRequestPage.tsx";
 import GiftCheckoutPage from "./pages/GiftCheckoutPage/GiftCheckoutPage.tsx";
 import FlowerRequestPage from "./pages/FlowerRequestPage/FlowerRequestPage.tsx";
 import FlowerCheckoutPage from "./pages/FlowerCheckoutPage/FlowerCheckoutPage.tsx";
-import SanitationRequestForm from "./components/Sanitation Request/SanitaitonRequestForm.tsx";
+import SanitationRequestForm from "./pages/SanitationRequest/SanitaitonRequestForm.tsx";
 
 function App() {
   const router = createBrowserRouter([
