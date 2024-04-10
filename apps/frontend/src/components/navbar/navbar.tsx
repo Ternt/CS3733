6d1c68ce79ba381190c7e378eef2dce0f0c778a1
Thinh Pham <kyle.pham202@gmail.com>
2024-04-10 01:19:01 -0400

    navbar with dropdown

--- apps/frontend/src/components/navbar/navbar.tsx
+++ apps/frontend/src/components/navbar/navbar.tsx
@@ -1,37 +1,51 @@
 import * as React from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+//import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
-import MenuItem from "@mui/material/MenuItem";
-import Menu from "@mui/material/Menu";
-
-import "./navbar.scss";
-import logo from "../../assets/logo_white_big.png";
+import { Menu, MenuItem } from "@mui/material";
+import Link from "@mui/material/Link";
 import { useNavigate } from "react-router-dom";
+// import Typography from "@mui/material/Typography";
+
+import "./NavBar.scss";
+import logo from "../../assets/logo_white_big.png";
 
 const pages = [
-  { label: "Map", path: "" },
-  { label: "Request Service", path: "/service-request" },
+  { label: "Map", path: "/" },
   { label: "Service List", path: "/service-request-display" },
   { label: "Node and Edge Tables", path: "/tables" },
 ];
 
-function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null,
-  );
+const services = [
+  { label: "Sanitation", path: "/sanitation" },
+  { label: "Medicine Delivery", path: "/medicine-request" },
+  { label: "Flowers", path: "/flower-request" },
+  { label: "Gift", path: "/gift-request" },
+];
 
+function ResponsiveAppBar() {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
   const navigate = useNavigate();
+
   const handleMenuItemClick = (path: string) => {
-    console.log("this ran");
     navigate(path);
   };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
+  const handleOnClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleClickMenuItemList = (path: string) => {
+    console.log(path);
+    navigate(path);
+    setAnchorEl(null);
   };
 
   return (
@@ -41,45 +55,20 @@ function ResponsiveAppBar() {
         bgcolor: "#012d5a",
       }}
     >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: "block", md: "none" },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem
-                  key={page.label}
-                  onClick={() => handleMenuItemClick(page.path)}
-                >
-                  <Typography textAlign="center">{page.label}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+      <Box>
+        <Toolbar>
+          {/* Logo */}
+          <Link href="" underline="none" sx={{ maxWidth: "30%" }}>
+            <Box
+              component="img"
+              className={"logo"}
+              src={logo}
+              alt={"logo"}
+              onClick={() => handleMenuItemClick("")}
+            ></Box>
+          </Link>
 
-          <img
-            className={"logo"}
-            src={logo}
-            alt={"logo"}
-            onClick={() => handleMenuItemClick("/login")}
-          />
-
+          {/* Menu Buttons */}
           <Box
             sx={{
               flexGrow: 1,
@@ -97,11 +86,10 @@ function ResponsiveAppBar() {
                   color: "white",
                   display: "block",
                   fontSize: 15,
-                  transition: "margin 0.2s ease-in-out",
+                  transition: "all 0.2s ease-in-out",
                   "&:hover": {
-                    mt: "18px",
-                    background: "#f6bd38",
-                    color: "black",
+                    transform: "translateY(-5px)",
+                    background: "#012d5a",
                   },
                 }}
               >
@@ -110,6 +98,48 @@ function ResponsiveAppBar() {
             ))}
 
             <Button
+              key={"Request Services"}
+              id="demo-customized-button"
+              aria-controls={open ? "demo-customized-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleOnClick}
+              sx={{
+                my: 3,
+                mr: 3,
+                height: 45,
+                color: "white",
+                display: "block",
+                fontSize: 15,
+                transition: "all 0.2s ease-in-out",
+                "&:hover": {
+                  transform: "translateY(-5px)",
+                  background: "#012d5a",
+                },
+              }}
+            >
+              {"Request Services"}
+            </Button>
+            <Menu
+              id="demo-customized-menu"
+              MenuListProps={{
+                "aria-labelledby": "demo-customized-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+            >
+              {services.map((services) => (
+                <MenuItem
+                  key={services.label}
+                  onClick={() => handleClickMenuItemList(services.path)}
+                  disableRipple
+                >
+                  {services.label}
+                </MenuItem>
+              ))}
+            </Menu>
+            <Button
               key={"login"}
               onClick={() => handleMenuItemClick("/login")}
               sx={{
@@ -117,13 +147,13 @@ function ResponsiveAppBar() {
                 height: 45,
                 paddingX: 5,
                 color: "black",
-                transition: "margin 0.2s ease-in-out",
+                transition: "all 0.2s ease-in-out",
                 fontSize: 15,
                 display: "block",
                 background: "#f6bd38",
 
                 "&:hover": {
-                  mt: "18px",
+                  transform: "translateY(-5px)",
                   background: "#f6bd38",
                   color: "black",
                 },
@@ -133,10 +163,9 @@ function ResponsiveAppBar() {
             </Button>
           </Box>
         </Toolbar>
-      </Container>
+      </Box>
     </AppBar>
   );
 }
 
 export default ResponsiveAppBar;
-//Husky Test2
