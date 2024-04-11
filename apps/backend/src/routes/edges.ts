3b03a6609d8556782a731efd69cc6239c4ccacb5
Alex <alexander.siracusa@gmail.com>
2024-04-11 13:35:24 -0400

    redid map upload endpoints

--- apps/backend/src/routes/edges.ts
+++ apps/backend/src/routes/edges.ts
@@ -8,67 +8,6 @@ import path from "path";
 
 const router: Router = express.Router();
 
-function splitLines(t: string): string[] {
-  return t.split(/\r\n|\r|\n/);
-}
-
-router.post("/upload/", async function (req: Request, res: Response) {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send("No files were uploaded.");
-    return;
-  }
-
-  // Get file data
-  const files = req.files;
-  const str: string = files.file.data.toString();
-  // ^ eslint does not like this line
-
-  // Check if headers are included in the file
-  let header: boolean = true;
-  if (req.query.header !== undefined) {
-    if (req.query.header!.toString() === "true") {
-      header = true;
-    } else if (req.query.header!.toString() === "false") {
-      header = false;
-    } else {
-      console.log("header must be 'true' or 'false'");
-      res.status(200);
-      return;
-    }
-  }
-
-  const prisma = new PrismaClient();
-
-  try {
-    // parse csv
-    const lines = splitLines(str).filter((line) => /\S/.test(line));
-    if (header) {
-      lines.shift();
-    }
-    const rows = lines.map((line) => line.split(","));
-
-    const data = Array.from(
-      rows.map((row) => ({
-        startNodeID: row[0],
-        endNodeID: row[1],
-        blocked: false,
-      })),
-    );
-
-    // upload node file
-    await prisma.$transaction([
-      prisma.EdgeDB.deleteMany(),
-      prisma.EdgeDB.createMany({ data }),
-    ]);
-  } catch {
-    console.log("edge file upload failed");
-    res.sendStatus(406);
-    return;
-  }
-
-  res.sendStatus(200);
-});
-
 router.get("/download/", async function (req: Request, res: Response) {
   const prisma = new PrismaClient();
   await exportEdgeDBToCSV(prisma, "../../map/edges.csv");
--- apps/backend/src/routes/map.ts
+++ apps/backend/src/routes/map.ts
@@ -1,6 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 import express, { Router, Request, Response } from "express";
-// import { Prisma } from "database";
-import PrismaClient from "../bin/database-connection.ts";
+import { PrismaClient } from "database";
+
+function splitLines(t: string): string[] {
+  return t.split(/\r\n|\r|\n/);
+}
 
 const router: Router = express.Router();
 
@@ -21,6 +27,94 @@ router.get("/", async function (req: Request, res: Response) {
 
   res.json({ nodes: nodes, edges: edges });
   res.end();
+});
+
+router.post("/upload", async function (req: Request, res: Response) {
+  if (!req.files || Object.keys(req.files).length < 2) {
+    res.status(400).send("No files were uploaded.");
+    return;
+  }
+
+  // Get file data
+  const files = req.files;
+  const node_str: string = files.nodes.data.toString();
+  const edge_str: string = files.edges.data.toString();
+  // ^ eslint does not like these lines
+
+  // Check if headers are included in the file
+  let header: boolean = true;
+  if (req.query.header !== undefined) {
+    if (req.query.header!.toString() === "true") {
+      header = true;
+    } else if (req.query.header!.toString() === "false") {
+      header = false;
+    } else {
+      console.log("header must be 'true' or 'false'");
+      res.status(200);
+      return;
+    }
+  }
+
+  const prisma = new PrismaClient();
+
+  try {
+    // parse node csv
+    const nodeLines = splitLines(node_str).filter((line) => /\S/.test(line));
+    if (header) {
+      nodeLines.shift();
+    }
+    const nodeRows = nodeLines.map((line) => line.split(","));
+
+    const nodeData = Array.from(
+      nodeRows.map((row) => ({
+        nodeID: row[0],
+        xcoord: parseInt(row[1]),
+        ycoord: parseInt(row[2]),
+        floor: row[3],
+        building: row[4],
+        nodeType: row[5],
+        longName: row[6],
+        shortName: row[7],
+      })),
+    );
+
+    // parse edges csv
+    const edgeLines = splitLines(edge_str).filter((line) => /\S/.test(line));
+    if (header) {
+      edgeLines.shift();
+    }
+    const edgeRows = edgeLines.map((line) => line.split(","));
+
+    const edgeData = Array.from(
+      edgeRows.map((row) => ({
+        startNodeID: row[0],
+        endNodeID: row[1],
+        blocked: false,
+      })),
+    );
+
+    const insertNodeQueries: PrismaPromise<R>[] = nodeData.map((row) => {
+      return prisma.nodeDB.create({ data: row });
+    });
+
+    const insertEdgeQueries: PrismaPromise<R>[] = edgeData.map((row) => {
+      return prisma.edgeDB.create({ data: row });
+    });
+
+    await prisma.$transaction([
+      prisma.$executeRaw`DELETE FROM "EdgeDB";`,
+      prisma.$executeRaw`DELETE FROM "NodeDB";`,
+      ...insertNodeQueries,
+      ...insertEdgeQueries,
+    ]);
+  } catch (error) {
+    console.log("node file upload failed");
+    console.log(error.message);
+    res.sendStatus(406);
+    return;
+  }
+
+  res.sendStatus(200);
 });
 
 export default router;
