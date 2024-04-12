e7cc006985bd2eeeb4dfc8eb60275b7785b33a53
Alex <alexander.siracusa@gmail.com>
2024-04-12 14:41:28 -0400

    refacoted service request endpoints

--- apps/backend/src/routes/service-requests.ts
+++ apps/backend/src/routes/service-requests.ts
@@ -1,89 +1,18 @@
 import express, {Router, Request, Response} from "express";
 import {Prisma, ServiceRequestType} from "database";
 import PrismaClient from "../bin/database-connection.ts";
+import createServiceRequestBody from "../helper/createServiceRequestBody.ts";
 
 const router: Router = express.Router();
 
 router.post("/", async function (req: Request, res: Response) {
-    const body = req.body;
-
-    // do not let id be manually specified
-    if (body.id !== undefined) {
-        console.error("Not allowed to specify request id. It is auto generated");
+    const body = createServiceRequestBody(req.body)
+    if (body === undefined) {
         res.sendStatus(400);
         return;
     }
 
-    // delete all detail parameters
-    // (Not meant be used from API)
-    delete body.sanitationDetail;
-    delete body.medicineDetail;
-    delete body.maintenanceDetail;
-    delete body.giftDetail;
-
-    // check for data corresponding to each service request type
-    switch (body.type) {
-        case "SANITATION":
-            body.sanitationDetail = {
-                create: {
-                    messType: body.messType,
-                    messSize: body.messSize,
-                },
-            };
-            delete body.messType;
-            delete body.messSize;
-            break;
-        case "MEDICINE":
-            body.medicineDetail = {
-                create: {
-                    patientName: body.patientName,
-                    primaryPhysicianName: body.primaryPhysicianName,
-                    medicine: body.medicine,
-                    dosage: body.dosage,
-                    form: body.form,
-                },
-            };
-            delete body.patientName;
-            delete body.primaryPhysicianName;
-            delete body.medicine;
-            delete body.dosage;
-            delete body.form;
-            break;
-        case "GIFT":
-            body.giftDetail = {
-                create: {
-                    senderName: body.senderName,
-                    recipientName: body.recipientName,
-                    shippingType: body.shippingType,
-
-                    cardNumber: body.cardNumber,
-                    cardCVV: body.cardCVV,
-                    cardHolderName: body.cardHolderName,
-                    cardExpirationDate: body.cardExpirationDate,
-
-                    items: {
-                        create: body.items.map((itemID: number) => {
-                            return {
-                                cartItemID: itemID,
-                            };
-                        }),
-                    },
-                },
-            };
-            delete body.senderName;
-            delete body.recipientName;
-            delete body.shippingType;
-
-            delete body.cardNumber;
-            delete body.cardCVV;
-            delete body.cardHolderName;
-            delete body.cardExpirationDate;
-
-            delete body.items;
-            break;
-    }
-
-    const serviceRequest: Prisma.ServiceRequestCreateInput = req.body;
+    const serviceRequest: Prisma.ServiceRequestCreateInput = body;
 
     try {
         // Attempt to create in the database
