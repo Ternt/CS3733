ee83d08340abf6b9352ce32c5ade2137b53bfcc0
Matt <47070087+Camo651@users.noreply.github.com>
2024-04-12 02:31:31 -0400

    deleted files

--- apps/frontend/src/pages/LoginPage/LoginPage.tsx
+++ apps/frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,10 +1,10 @@
 import "./LoginPage.scss";
-import Card from "../../components/Card/Card.tsx";
 import { useEffect, useState } from "react";
 import { LoginCredentials } from "../../common/LoginCredentials.ts";
 import { useNavigate } from "react-router-dom";
 import { Box, Button, FormControl, TextField } from "@mui/material";
 import background from "./login-page-background.png";
+import Card from "@mui/material/Card";
 
 export default function LoginPage() {
   useEffect(() => {
