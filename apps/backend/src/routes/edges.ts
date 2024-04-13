87f8318ddcaf6bc38371a965c80725b9b47dba62
Alex <alexander.siracusa@gmail.com>
2024-04-13 00:10:22 -0400

    can add edges

--- apps/backend/src/routes/edges.ts
+++ apps/backend/src/routes/edges.ts
@@ -2,7 +2,7 @@
 // @ts-nocheck
 
 import express, { Router, Request, Response } from "express";
-import { PrismaClient } from "database";
+import {Prisma, PrismaClient} from "database";
 import { exportEdgeDBToCSV } from "../helper/manageDatabases";
 import path from "path";
 
@@ -12,6 +12,48 @@ router.get("/download/", async function (req: Request, res: Response) {
   const prisma = new PrismaClient();
   await exportEdgeDBToCSV(prisma, "../../map/temp/edgesDownload.csv");
   res.download(path.join(__dirname, "../../map/temp/edgesDownload.csv"));
+});
+
+router.put("/update/", async function (req: Request, res: Response) {
+    const prisma = new PrismaClient();
+    const edge: Prisma.EdgeDBCreateInput = req.body;
+
+    try {
+        // check if edge in the reverse direction exists
+        // if so flip start/end to match it
+        const reverse = await prisma.edgeDB.findUnique({
+            where: {
+                edgeID: {
+                    startNodeID: edge.endNodeID,
+                    endNodeID: edge.startNodeID,
+                }
+            }
+        })
+        if (reverse != null) {
+            const temp = edge.startNodeID;
+            edge.startNodeID = edge.endNodeID;
+            edge.endNodeID = temp;
+        }
+
+        // try to add/update edge in the database
+        await prisma.edgeDB.upsert({
+            where: {
+                edgeID: {
+                    startNodeID: edge.startNodeID,
+                    endNodeID: edge.endNodeID,
+                }
+            },
+            update: edge,
+            create: edge,
+        })
+    }
+    catch (error) {
+        console.error(error.message);
+        res.sendStatus(400);
+        return;
+    }
+
+    res.sendStatus(200);
 });
 
 export default router;
