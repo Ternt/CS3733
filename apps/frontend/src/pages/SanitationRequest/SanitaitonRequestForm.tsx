ae462de36154486e9dc8639f9b52b5e14cdc6c32
Thinh Pham <kyle.pham202@gmail.com>
2024-04-14 15:59:37 -0400

    Copied stuff from medicine delivery over to sanitation.

--- apps/frontend/src/pages/SanitationRequest/SanitaitonRequestForm.tsx
+++ apps/frontend/src/pages/SanitationRequest/SanitaitonRequestForm.tsx
@@ -1,19 +1,21 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import {
-  TextField,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-  FormControl,
-  MenuItem,
-  Button,
-  Box,
-  Typography,
-  FormLabel,
+    TextField,
+    RadioGroup,
+    FormControlLabel,
+    Radio,
+    FormControl,
+    MenuItem,
+    Button,
+    Box,
+    Typography,
+    FormLabel, InputAdornment, IconButton, Grow,
 } from "@mui/material";
 
 import Checkboxes from "../../components/FormElements/Checkboxes.tsx";
 import LocationDropdown from "../../components/LocationDropdown.tsx";
+import EventIcon from "@mui/icons-material/Event";
+import Calendar from "../../components/Calendar/Calendar.tsx";
 
 type formFields = {
   name: string;
@@ -29,14 +31,17 @@ function SanitationRequestForm() {
     document.title = "Sanitation Request";
   });
 
-  const [formInput, setFormInput] = useState<formFields>({
-    name: "",
-    priority: "",
-    location: "",
-    type: [],
-    size: "",
-    status: "",
-  });
+
+    const [calendarMenuFlag, setCalendarMenuFlag] = useState<boolean>(true);
+    const [formMenuTransform, setFormMenuTransform] = useState<number>(150);
+    const [formInput, setFormInput] = useState<formFields>({
+        name: "",
+        priority: "",
+        location: "",
+        type: [],
+        size: "",
+        status: "",
+    });
 
   function handleNameInput(e: ChangeEvent<HTMLInputElement>) {
     setFormInput({ ...formInput, name: e.target.value });
@@ -140,206 +145,265 @@ function SanitationRequestForm() {
   }
 
   return (
-    <Box
-      position="relative"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        height: "100vh",
-        backgroundSize: "cover",
-      }}
-    >
-      <Box
-        position="absolute"
-        top={70}
-        sx={{
-          width: "500px",
-          display: "flex",
-          justifyContent: "center",
-          backgroundColor: "#012d5a",
-          color: "#f6bd38",
-          p: 2,
-          borderRadius: "23px 23px 0 0",
-        }}
-      >
-        <Typography
-          style={{ fontFamily: "Open Sans", fontWeight: 600 }}
-          variant="h4"
-          component="h1"
-          align="center"
-        >
-          SANITATION REQUEST
-        </Typography>
-      </Box>
-
-      <Box
-        position="absolute"
-        top={150}
-        sx={{
-          width: "500px",
-          backgroundColor: "whitesmoke",
-          borderRadius: "0 0 23px 23px ",
-            boxShadow: 3,
-        }}
-      >
-          {/*<Box*/}
-          {/*    sx={{*/}
-          {/*        display: "flex",*/}
-          {/*        justifyContent: "center",*/}
-          {/*    }}*/}
-          {/*>*/}
-          {/*    <Typography>Made by Yuhan & Warwick</Typography>*/}
-          {/*</Box>*/}
-
-        <form
-          id="sanitationForm"
-          style={{
-            backgroundColor: "whitesmoke",
-            display: "flex",
-            justifyContent: "center",
-            borderRadius: "0 0 23px 23px ",
-          }}
-        >
-          <FormControl
+     <Box
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "top",
-                padding: 2,
-                gap: 1,
-            }}
-          >
-            <TextField
-              required
-              label="Employee Name"
-              onChange={handleNameInput}
-              margin="normal"
-              value={formInput.name}
-              fullWidth
-              sx={{marginY: 0}}
-            />
-
-            <TextField
-              required
-              select
-              id="priority-select"
-              label={"Priority"}
-              margin="normal"
-              inputProps={{ MenuProps: { disableScrollLock: true } }}
-              value={formInput.priority}
-              onChange={(event) => {
-                setFormInput({
-                  ...formInput,
-                  priority: event.target.value,
-                });
-              }}
-              sx={{marginY: 0}}
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'center',
+            }}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    position: 'relative',
+                    m: '3%',
+                    mt: '6%',
+                    width: '30%',
+                    transform: `translate(${formMenuTransform}px)`,
+                    transition: '0.5s',
+                    transitionDelay: (calendarMenuFlag? '100ms':'0ms'),
+                }}
             >
-              <MenuItem value={"LOW"}>Low</MenuItem>
-              <MenuItem value={"MEDIUM"}>Medium</MenuItem>
-              <MenuItem value={"HIGH"}>High</MenuItem>
-              <MenuItem value={"EMERGENCY"}>Emergency</MenuItem>
-            </TextField>
+                <Box
+                    sx={{
+                        width: '100%',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        backgroundColor: '#012d5a',
+                        color: '#f6bd38',
+                        p: 2,
+                        borderRadius: '23px 23px 0 0',
+                    }}
+                >
+                <Typography
+                        style={{fontFamily: 'Open Sans', fontWeight: 600}}
+                        variant="h4"
+                        component="h1"
+                        align="center"
+                    >
+                        SANITATION REQUEST
+                </Typography>
+            </Box>
 
-              <Box sx={{marginY: 0}}><LocationDropdown
-                  onChange={(v: string) => {
-                      setFormInput({ ...formInput, location: v });
-                  }}
-                  value={formInput.location}
-                  filterTypes={["HALL"]}
-                  label={"Location"}
-              /></Box>
+             <Box
+                 sx={{
+                     width: '100%',
+                     backgroundColor: 'whitesmoke',
+                     borderRadius: '0 0 23px 23px',
+                     boxShadow: 3,
+                 }}
+             >
+                 <form
+                     id="sanitationform"
+                     style={{
+                         backgroundColor: 'whitesmoke',
+                         display: 'flex',
+                         justifyContent: 'center',
+                         borderRadius: '0 0 23px 23px',
+                         flexDirection: 'column',
+                         margin: '2%',
+                     }}
+                 >
+                    <FormControl
+                        sx={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "top",
+                            padding: 2,
+                            gap: 1,
+                        }}
+                    >
+                        <TextField
+                            required
+                            label="Employee Name"
+                            onChange={handleNameInput}
+                            margin="normal"
+                            value={formInput.name}
+                            fullWidth
+                            sx={{marginY: 0}}
+                        />
 
-            <Box sx={{marginY: 0}}><Checkboxes
-              label={"Mess Type"}
-              onChange={handleTypeChange}
-              items={["Solid Waste", "Liquid Spill", "Other"]}
-              checked={formInput.type}
-            /></Box>
+                        <TextField
+                            required
+                            select
+                            id="priority-select"
+                            label={"Priority"}
+                            margin="normal"
+                            inputProps={{ MenuProps: { disableScrollLock: true } }}
+                            value={formInput.priority}
+                            onChange={(event) => {
+                                setFormInput({
+                                    ...formInput,
+                                    priority: event.target.value,
+                                });
+                            }}
+                            sx={{marginY: 0}}
+                        >
+                            <MenuItem value={"LOW"}>Low</MenuItem>
+                            <MenuItem value={"MEDIUM"}>Medium</MenuItem>
+                            <MenuItem value={"HIGH"}>High</MenuItem>
+                            <MenuItem value={"EMERGENCY"}>Emergency</MenuItem>
+                        </TextField>
 
-            <Box sx={{marginY: 0}}><FormLabel id="mess-size">Mess Size</FormLabel>
-            <RadioGroup
-              row
-              name="mess-size"
-              aria-labelledby="mess-size"
-              value={formInput.size}
-              onChange={updateSizeInput}
-            >
-              <FormControlLabel
-                value="SMALL"
-                control={<Radio />}
-                label="Small"
+                        <Box sx={{marginY: 0}}><LocationDropdown
+                            onChange={(v: string) => {
+                                setFormInput({ ...formInput, location: v });
+                            }}
+                            value={formInput.location}
+                            filterTypes={["HALL"]}
+                            label={"Location"}
+                        /></Box>
 
-              />
-              <FormControlLabel
-                value="MEDIUM"
-                control={<Radio />}
-                label="Medium"
-              />
-              <FormControlLabel
-                value="LARGE"
-                control={<Radio />}
-                label="Large"
-              />
-            </RadioGroup></Box>
+                        <Box>
+                            <TextField
+                                label={"Date"}
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <IconButton onClick={() => {
+                                                setCalendarMenuFlag(!calendarMenuFlag);
+                                                setFormMenuTransform((calendarMenuFlag?0:150));
+                                                console.log(calendarMenuFlag);
+                                            }}>
+                                                <EventIcon/>
+                                            </IconButton>
+                                        </InputAdornment>
+                                    ),
+                                }}
+                                sx={{width:"100%"}}
+                            />
+                        </Box>
 
-            <TextField
-              required
-              select
-              value={formInput.status}
-              label={"Status"}
-              margin="normal"
-              inputProps={{ MenuProps: { disableScrollLock: true } }}
-              onChange={(event) => {
-                setFormInput({
-                  ...formInput,
-                  status: event.target.value,
-                });
-              }}
-              sx={{marginY: 0}}
-            >
-              <MenuItem value={"UNASSIGNED"}>Unassigned</MenuItem>
-              <MenuItem value={"ASSIGNED"}>Assigned</MenuItem>
-              <MenuItem value={"IN_PROGRESS"}>In Progress</MenuItem>
-              <MenuItem value={"CLOSED"}>Closed</MenuItem>
-            </TextField>
+                        <Box sx={{marginY: 0}}><Checkboxes
+                            label={"Mess Type"}
+                            onChange={handleTypeChange}
+                            items={["Solid Waste", "Liquid Spill", "Other"]}
+                            checked={formInput.type}
+                        /></Box>
 
-              <Box
-                  sx={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      marginY: 0,
-                  }}
-              >
-                  <Button
-                      type="button"
-                      variant="contained"
-                      color="secondary"
-                      sx={{
-                          margin: 1,
-                      }}
-                      onClick={clearForm}
-                  >
-                      Clear
-                  </Button>
+                        <Box sx={{marginY: 0}}><FormLabel id="mess-size">Mess Size</FormLabel>
+                            <RadioGroup
+                                row
+                                name="mess-size"
+                                aria-labelledby="mess-size"
+                                value={formInput.size}
+                                onChange={updateSizeInput}
+                            >
+                                <FormControlLabel
+                                    value="SMALL"
+                                    control={<Radio />}
+                                    label="Small"
 
-                  <Button
-                      type="button"
-                      variant="contained"
-                      color="secondary"
-                      sx={{
-                          margin: 1,
-                      }}
-                      disabled={!isComplete()}
-                      onClick={submitForm}
-                  >
-                      Submit
-                  </Button>
-              </Box>
-          </FormControl>
-        </form>
-      </Box>
+                                />
+                                <FormControlLabel
+                                    value="MEDIUM"
+                                    control={<Radio />}
+                                    label="Medium"
+                                />
+                                <FormControlLabel
+                                    value="LARGE"
+                                    control={<Radio />}
+                                    label="Large"
+                                />
+                            </RadioGroup></Box>
 
+                        <TextField
+                            required
+                            select
+                            value={formInput.status}
+                            label={"Status"}
+                            margin="normal"
+                            inputProps={{ MenuProps: { disableScrollLock: true } }}
+                            onChange={(event) => {
+                                setFormInput({
+                                    ...formInput,
+                                    status: event.target.value,
+                                });
+                            }}
+                            sx={{marginY: 0}}
+                        >
+                            <MenuItem value={"UNASSIGNED"}>Unassigned</MenuItem>
+                            <MenuItem value={"ASSIGNED"}>Assigned</MenuItem>
+                            <MenuItem value={"IN_PROGRESS"}>In Progress</MenuItem>
+                            <MenuItem value={"CLOSED"}>Closed</MenuItem>
+                        </TextField>
+
+                        <Box
+                            sx={{
+                                display: "flex",
+                                justifyContent: "space-between",
+                                marginY: 0,
+                            }}
+                        >
+                            <Button
+                                type="button"
+                                variant="contained"
+                                color="secondary"
+                                sx={{
+                                    margin: 1,
+                                }}
+                                onClick={clearForm}
+                            >
+                                Clear
+                            </Button>
+
+                            <Button
+                                type="button"
+                                variant="contained"
+                                color="secondary"
+                                sx={{
+                                    margin: 1,
+                                }}
+                                disabled={!isComplete()}
+                                onClick={submitForm}
+                            >
+                                Submit
+                            </Button>
+                        </Box>
+                    </FormControl>
+                </form>
+            </Box>
+        </Box>
+
+         <Box sx={{mt: '6%'}}>
+             <Grow in={!calendarMenuFlag} {...!calendarMenuFlag? {timeout:1000}:{}}>{
+                 <Box>
+                     <Box
+                         sx={{
+                             width: '100%',
+                             display: 'flex',
+                             justifyContent: 'center',
+                             backgroundColor: '#012d5a',
+                             color: '#f6bd38',
+                             p: 2,
+                             borderRadius: '23px 23px 0 0',
+                         }}
+                     >
+                         <Typography
+                             style={{fontFamily: 'Open Sans', fontWeight: 600}}
+                             variant="h4"
+                             component="h1"
+                             align="center"
+                         >
+                             Date
+                         </Typography>
+                     </Box>
+                     <Box
+                         sx={{
+                             backgroundColor: 'whitesmoke',
+                             borderRadius: '0 0 23px 23px',
+                             boxShadow: 3,
+                             padding: '1%',
+                             display: 'flex',
+                             justifyContent: 'center',
+                         }}
+                     >
+                         <Calendar/>
+                     </Box>
+                 </Box>
+             }
+             </Grow>
+         </Box>
     </Box>
   );
 }
