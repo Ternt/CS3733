7676ce659783e318752093dafd7157770b61d3e7
Abysssigher <80065903+Abysssigher@users.noreply.github.com>
2024-04-15 17:44:15 -0400

    natural language direction feature done, need to work on make it look nicer

--- apps/frontend/src/components/NaturalLanguageDirection/NaturalLanguageDirection.tsx
+++ apps/frontend/src/components/NaturalLanguageDirection/NaturalLanguageDirection.tsx
@@ -14,6 +14,7 @@ async function getLanguageDirection(path: Array<string>){
 
     for (let i = 0; i < path.length - 1; i++) {
         const currentNode = nodeTable.find(nodes => nodes.nodeID === path[i]);
+        const currentNodeName = currentNode.shortName;
         nodeList.push(currentNode);
         const nextNode = nodeTable.find(nodes => nodes.nodeID === path[i+1]);
 
@@ -23,22 +24,25 @@ async function getLanguageDirection(path: Array<string>){
         else {
             const dx = nextNode.xcoord - currentNode.xcoord;
             const dy = nextNode.ycoord - currentNode.ycoord;
-            const angle = Math.atan2(dx, dy);
+            const angle = Math.atan2(dy, dx);
 
             if (i > 0) {
                 const previousNode = nodeTable.find(nodes => nodes.nodeID === path[i-1]);
                 const dxPrev = currentNode.xcoord - previousNode.xcoord;
                 const dyPrev = currentNode.ycoord - previousNode.ycoord;
-                const anglePrev = Math.atan2(dxPrev, dyPrev);
+
+                const anglePrev = Math.atan2(dyPrev, dxPrev);
 
                 const directionChange = angle - anglePrev;
                 console.log(directionChange);
                 if (Math.abs(directionChange) < Math.PI / 4) {
-                    directions.push("Walk straight");
+                    if (directions[directions.length - 1] != "Walk straight") {
+                        directions.push("Walk straight");
+                    }
                 } else if (directionChange > 0) {
-                    directions.push("Turn right");
+                    directions.push("Turn right at"+ currentNodeName);
                 } else {
-                    directions.push("Turn left");
+                    directions.push("Turn left at:" + currentNodeName);
                 }
             }
         }}
@@ -88,7 +92,7 @@ function NaturalLanguageDirection({startLocation, endLocation}) {
     return (
         <Box sx={{ p: 2, width: "100%"}}>
             <Typography>
-                Path from {startLocation} to {endLocation}: {directionData.join(", ")}
+                Path from {startLocation} to {endLocation}: {directionData.join(", \n")}
             </Typography>
         </Box>
     );
