eac11caeb7c55edef243dc9f27380fe8c470bd13
Thinh Pham <kyle.pham202@gmail.com>
2024-04-15 18:34:31 -0400

    Service Request admin hub now displays all service requests in the database.

--- apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
+++ apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
@@ -1,68 +1,90 @@
 import {Box, Paper, Typography} from "@mui/material";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {RequestInspectionDialogue} from "./RequestInspectionDialogue.tsx";
+import axios, {AxiosResponse} from "axios";
 
 export type RequestCard = {
-  type:string;
+    type:string;
+}
+
+
+type serviceRequest = {
+    requestID: number,
+    assignedEmployee: string,
+    priority: string,
+    status: string,
+    type: string,
+    giftDetail? : string,
+    maintenanceDetail?: string,
+    medicineDetail?: string,
+    notes?: string,
 }
 
 export default function ServiceRequestOverview(){
 
-  const [selectedCard, setSelectedCard] = useState<RequestCard | null>(null);
-  const reqs = ["Service 1", "Service 2", "Serice 3", "AAAAAA", "AAAAAA"];
+    const [selectedCard, setSelectedCard] = useState<RequestCard | null>(null);
+    const [reqs, setReqs] = useState<serviceRequest[]>([]);
 
-  return(
-    <>
-      <Box
-        sx={{
-          width:'100%',
-          overflowY:'scroll',
-          display:'flex',
-          flexDirection:'row',
-          justifyContent:'space-evenly',
-          alignItems:'flex-start',
-          pt:3,
-        }}
-      >
-        {
-          reqs.map((service)=>{
-          return (
+    useEffect(()=>{
+        axios.get('/api/service-requests').then((res: AxiosResponse) => {
+            setReqs(res.data);
+        });
+    }, []);
+
+
+    return(
+        <>
             <Box
-              sx={{
-                width: ((100.0/reqs.length - 2)+"%"),
-                minHeight:'85vh',
-                height: 'fill-available',
-                display:'flex',
-                flexDirection:'column',
-                justifyContent:'flex-start',
-                alignItems:'flex-start',
-                gap:'1rem',
-              }}
-            >
-              <Typography variant={"h4"}>{service}</Typography>
-              <Paper
                 sx={{
-                  width:'100%',
-                  height:'300px',
-                  bgcolor:'white'
+                    width:'100%',
+                    overflowY:'scroll',
+                    display:'flex',
+                    flexDirection:'row',
+                    justifyContent:'space-evenly',
+                    alignItems:'flex-start',
+                    pt:3,
                 }}
-                elevation={5}
-              >
+            >
+                {
+                    reqs.map((service)=>{
+                        return (
+                            <Box
+                                key={service.requestID}
+                                sx={{
+                                    width: ((100.0/reqs.length - 2)+"%"),
+                                    minHeight:'85vh',
+                                    height: 'fill-available',
+                                    display:'flex',
+                                    flexDirection:'column',
+                                    justifyContent:'flex-start',
+                                    alignItems:'flex-start',
+                                    gap:'1rem',
+                                }}
+                            >
+                                <Typography variant={"h4"}>{service.type}</Typography>
+                                <Paper
+                                    sx={{
+                                        width:'100%',
+                                        height:'300px',
+                                        bgcolor:'white'
+                                    }}
+                                    elevation={5}
+                                >
 
-              </Paper>
+                                </Paper>
+
+                            </Box>
+                        );})
+                }
+
 
             </Box>
-          );})
-        }
-
-
-      </Box>
-      <RequestInspectionDialogue
-        selectedRequest={selectedCard}
-        onCloseDialogue={()=>{
-          setSelectedCard(null);
-        }}
-      />
-    </>
-  );
+            <RequestInspectionDialogue
+                selectedRequest={selectedCard}
+                onCloseDialogue={()=>{
+                    setSelectedCard(null);
+                }}
+            />
+        </>
+    );
 }
