75a8584ee8b1b785b80efd9909c62741e48cf59d
Alex <alexander.siracusa@gmail.com>
2024-04-16 22:10:42 -0400

    no more prisma clients

--- apps/backend/src/routes/nodes.ts
+++ apps/backend/src/routes/nodes.ts
@@ -2,24 +2,22 @@
 // @ts-nocheck
 
 import express, { Router, Request, Response } from "express";
-import {Prisma, PrismaClient} from "database";
+import PrismaClient from "../bin/database-connection.ts";
 import path from "path";
 import { exportNodeDBToCSV } from "../helper/manageDatabases.ts";
 
 const router: Router = express.Router();
 
 router.get("/download/", async function (req: Request, res: Response) {
-  const prisma = new PrismaClient();
-  await exportNodeDBToCSV(prisma, "../../map/temp/nodesDownload.csv");
+  await exportNodeDBToCSV("../../map/temp/nodesDownload.csv");
   res.download(path.join(__dirname, "../../map/temp/nodesDownload.csv"));
 });
 
 router.put("/update/", async function (req: Request, res: Response) {
-    const prisma = new PrismaClient();
-    const node: Prisma.NodeDBCreateInput = req.body;
+    const node: PrismaClient.NodeDBCreateInput = req.body;
 
     try {
-        await prisma.nodeDB.upsert({
+        await PrismaClient.nodeDB.upsert({
             where: {
                 nodeID: node.nodeID,
             },
@@ -37,7 +35,6 @@ router.put("/update/", async function (req: Request, res: Response) {
 });
 
 router.delete("/delete/", async function (req: Request, res: Response) {
-    const prisma = new PrismaClient();
     if (req.query.nodeID === undefined) {
         console.error("nodeID to delete must be specified");
         res.sendStatus(400);
@@ -45,7 +42,7 @@ router.delete("/delete/", async function (req: Request, res: Response) {
     }
 
     try {
-        await prisma.nodeDB.delete({
+        await PrismaClient.nodeDB.delete({
             where: {
                 nodeID: req.query.nodeID,
             }
