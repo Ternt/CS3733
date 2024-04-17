b8a9fda930f240b6032edbfed5608aad69320894
Thinh Pham <kyle.pham202@gmail.com>
2024-04-16 20:46:27 -0400

    Made each category scrollable when there's too many service requests

--- apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
+++ apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
@@ -40,7 +40,7 @@ export default function ServiceRequestOverview(){
 
     const [selectedCard, setSelectedCard] = useState<RequestCard | null>(null);
     const [reqs, setReqs] = useState<serviceRequest[]>([]);
-    const reqTypes = ["MEDICINE", "SANITATION", "GIFT", "DELIVERY"];
+    const reqTypes = ["MEDICINE", "SANITATION", "GIFT", "FLOWER"];
 
     useEffect(()=>{
         axios.get('/api/service-requests').then((res: AxiosResponse) => {
@@ -59,114 +59,116 @@ export default function ServiceRequestOverview(){
         <>
             <Box
                 sx={{
+                    height: "90vh",
                     width:'100%',
-                    overflowY:'scroll',
+                    overflowY:'hidden',
+                    p: 1,
                     display:'flex',
                     flexDirection:'row',
                     justifyContent:'space-evenly',
                     alignItems:'flex-start',
-                    pt:3,
-                    height: "90vh",
+                    backgroundColor: '#E4E4E4',
                 }}
             >
-                {
-                    reqTypes.map((category)=>{
-                        return (
-                            <Box
-                                key={category}
-                                sx={{
-                                    width: ((100.0/reqTypes.length - 2)+"%"),
-                                    minHeight:'85vh',
-                                    height: 'fill-available',
-                                    display:'flex',
-                                    flexDirection:'column',
-                                    justifyContent:'flex-start',
-                                    alignItems:'flex-start',
-                                }}
-                            >
-                                <Typography
-                                    variant={"h4"}
-                                    sx={{
-                                        display: "flex",
-                                        justifyContent: "center",
-                                        alignItem: "center",
-                                    }}>{category}</Typography>
+                    {
+                        reqTypes.map((category)=>{
+                            return (
                                 <Box
+                                    key={category}
                                     sx={{
-                                        display: "flex",
-                                        flexDirection: "column",
-                                        borderRadius: "1%",
-                                        padding: '1.5%',
-                                        minHeight:'80vh',
-                                        height: 'fill-available',
-                                        width: '100%',
-                                        backgroundColor: '#E4E4E4',
-                                        gap: "1rem",
-                                        boxShadow: 3,
-                                    }}>
-                                {
-                                    reqs.map((service) => {
-                                        return(
-                                            ((service.type === category)?
-                                                <Paper
-                                                    key={service.requestID}
-                                                    sx={{
-                                                        width:'100%',
-                                                        bgcolor:'#FFFFFF',
-                                                        // flexGrow: 1,
-                                                    }}
-                                                    elevation={1}
-                                                >
-                                                    <Box sx={{ display: 'flex', flexDirection: 'column', px: "5%", pt: "5%", pb: "2.5%" }}>
-                                                        <Typography variant="h6">{service.location.longName}</Typography>
-                                                        <Typography>ID {service.location.nodeID}</Typography>
-                                                    </Box>
-                                                    <Box
-                                                        sx={{
-                                                            backgroundColor: "#F1F1F1",
-                                                            borderRadius: 1,
-                                                        }}>
-                                                        <Box sx={{px: "5%", pb: "5%", pt: "2.5%"}}>
-                                                            <Box>
-                                                                <FormControl
-                                                                    sx={{gap: 1, width: "100%",}}>
-                                                                    <TextField
-                                                                        select
-                                                                        value={service.status}
-                                                                        margin="normal"
-                                                                        inputProps={{MenuProps: {disableScrollLock: true}}}
-                                                                        sx={{marginY: 0, width: "100%"}}
-                                                                        onChange={onChangeStatus}
-                                                                    >
-                                                                        <MenuItem value={"UNASSIGNED"}>Unassigned</MenuItem>
-                                                                        <MenuItem value={"ASSIGNED"}>Assigned</MenuItem>
-                                                                        <MenuItem value={"IN_PROGRESS"}>In Progress</MenuItem>
-                                                                        <MenuItem value={"CLOSED"}>Closed</MenuItem>
-                                                                    </TextField>
-                                                                </FormControl>
+                                        width: ((100.0/reqTypes.length - 1)+"%"),
+                                        minHeight:'85vh',
+                                        display:'flex',
+                                        flexDirection:'column',
+                                        justifyContent:'flex-start',
+                                        alignItems:'flex-start',
+                                        p: "0.5%",
+                                    }}
+                                >
+                                    <Typography
+                                        variant={"h4"}
+                                        sx={{
+                                            display: "flex",
+                                            justifyContent: "center",
+                                            alignItem: "center",
+                                            px: "5%",
+                                            pt: "4%",
+                                            pb: "2%",
+                                        }}>{category}
+                                    </Typography>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            width: '100%',
+                                            minHeight:'80vh',
+                                            height: '80vh',
+                                            gap: "1rem",
+                                            overflow: 'scroll',
+                                            p: '5%',
+                                        }}>
+                                        {
+                                            reqs.map((service) => {
+                                                return(
+                                                    ((service.type === category)?
+                                                        <Paper
+                                                            key={service.requestID}
+                                                            sx={{
+                                                                width:'100%',
+                                                                backgroundColor:'#FFFFFF',
+                                                                borderRadius: 2,
+                                                                // flexGrow: 1,
+                                                            }}
+                                                            elevation={1}
+                                                        >
+                                                            <Box sx={{ display: 'flex', flexDirection: 'column', px: "5%", pt: "5%", pb: "2.5%" }}>
+                                                                <Typography variant="h6">{service.location.longName}</Typography>
+                                                                <Typography>ID {service.location.nodeID}</Typography>
                                                             </Box>
-                                                        </Box>
-                                                    </Box>
-                                                </Paper> : <></>)
-                                        );
-                                    })
-                                }
-                                {/*<Box sx={{display: 'flex'}}>*/}
-                                {/*    <Button*/}
-                                {/*        variant="contained"*/}
-                                {/*        sx={{*/}
-                                {/*            width: "20vw",*/}
-                                {/*        }}*/}
-                                {/*    >*/}
-                                {/*        <AddIcon/>*/}
-                                {/*    </Button>*/}
-                                {/*</Box>*/}
+                                                            <Box
+                                                                sx={{
+                                                                    backgroundColor: "#F1F1F1",
+                                                                    borderRadius: 2,
+                                                                }}>
+                                                                <Box sx={{px: "5%", pb: "5%", pt: "2.5%"}}>
+                                                                    <Box>
+                                                                        <FormControl
+                                                                            sx={{width: "100%",}}>
+                                                                            <TextField
+                                                                                select
+                                                                                value={service.status}
+                                                                                margin="normal"
+                                                                                inputProps={{MenuProps: {disableScrollLock: true}}}
+                                                                                sx={{marginY: 0, width: "100%"}}
+                                                                                onChange={onChangeStatus}
+                                                                            >
+                                                                                <MenuItem value={"UNASSIGNED"}>Unassigned</MenuItem>
+                                                                                <MenuItem value={"ASSIGNED"}>Assigned</MenuItem>
+                                                                                <MenuItem value={"IN_PROGRESS"}>In Progress</MenuItem>
+                                                                                <MenuItem value={"CLOSED"}>Closed</MenuItem>
+                                                                            </TextField>
+                                                                        </FormControl>
+                                                                    </Box>
+                                                                </Box>
+                                                            </Box>
+                                                        </Paper> : <></>)
+                                                );
+                                            })
+                                        }
+                                        {/*<Box sx={{display: 'flex'}}>*/}
+                                        {/*    <Button*/}
+                                        {/*        variant="contained"*/}
+                                        {/*        sx={{*/}
+                                        {/*            width: "20vw",*/}
+                                        {/*        }}*/}
+                                        {/*    >*/}
+                                        {/*        <AddIcon/>*/}
+                                        {/*    </Button>*/}
+                                        {/*</Box>*/}
+                                    </Box>
                                 </Box>
-                            </Box>
-                        );})
-                }
-
-
+                            );})
+                    }
             </Box>
             <RequestInspectionDialogue
                 selectedRequest={selectedCard}
