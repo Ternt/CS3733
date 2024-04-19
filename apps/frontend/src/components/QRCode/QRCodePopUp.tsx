fef4e8bd7383a38b29e73a317b6886f3f6b215ea
zachszeto <zacharyszto@gmail.com>
2024-04-19 12:25:11 -0400

    Connected QRCode to Our Mobile Directions Page

--- apps/frontend/src/components/QRCode/QRCodePopUp.tsx
+++ apps/frontend/src/components/QRCode/QRCodePopUp.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from '@mui/material';
 import Box from "@mui/material/Box";
 import QRCode from "qrcode";
@@ -11,29 +11,35 @@ type QRCodePopUpProp = {
 
 export default function QRCodePopUp(props: QRCodePopUpProp) {
     const [showQRCode, setShowQRCode] = useState(false);
-
+    const [imgSource, setImgSource] = useState("");
     const toggleQRCode = () => {
         setShowQRCode(!showQRCode);
     };
 
-    const [imgSource, setImgSource] = useState("");
+    useEffect(() => {
+        const url = `ec2-18-217-227-54.us-east-2.compute.amazonaws.com/directions?startLocation=${props.startNode}&endLocation=${props.endNode}&algo=${props.algo}`;
+        const generateQRCode = () => {
+            QRCode.toDataURL(url, (err, url) => {
+                if (!err) {
+                    setImgSource(url);
+                }
+            });
+        };
 
-    QRCode.toDataURL('/directions?startLocation=' + props.startNode + "&endLocation=" + props.endNode + "&algo=" + props.algo, function (err, url) {
-        if (err) {
-            return err;
-        }
-        setImgSource(url);
-    });
+        generateQRCode();
+    }, [props.startNode, props.endNode, props.algo]);
 
     return (
-        <Box>
+        <Box sx={{
+            width: '50%',
+        }}>
             <Button
                 onClick={toggleQRCode}
                 sx={{
                     backgroundColor: '#012d5a',
                     color: 'white',
                     height: '100%',
-                    width: '50%',
+                    width: '100%',
                     display: 'flex',
                     alignSelf: 'center',
                     "&:hover": {
@@ -50,10 +56,13 @@ export default function QRCodePopUp(props: QRCodePopUpProp) {
                     src={imgSource}
                     alt={"QR Code"}
                     sx={{
-                        height: "10vh",
-                        width: "10vh",
+                        height: "50vh",
+                        width: "50vh",
                         mx: 1,
                         p: "1%",
+                        position: 'absolute',
+                        top: '30%',
+                        left: '40%',
                     }}
                 ></Box>
             )}
