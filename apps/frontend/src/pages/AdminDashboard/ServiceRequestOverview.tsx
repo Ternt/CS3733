92d5775ecfdc0048cf6385c9ceb717e2a1589c1e
Thinh Pham <kyle.pham202@gmail.com>
2024-04-20 14:20:14 -0400

    WIP Filter functionality

--- apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
+++ apps/frontend/src/pages/AdminDashboard/ServiceRequestOverview.tsx
@@ -5,114 +5,128 @@ import axios, {AxiosResponse} from "axios";
 import KanbanBoardCard from "./KanbanBoardCard.tsx";
 import {AssignedEmployee, ServiceRequest} from "../../helpers/typestuff.ts";
 import * as React from "react";
-import {EmployeeAutocompleteOption} from "../../components/EmployeeAutoComplete.tsx";
+import EmployeeAutoComplete, {EmployeeAutocompleteOption} from "../../components/EmployeeAutoComplete.tsx";
 // import {RequestInspectionDialogue} from "./RequestInspectionDialogue.tsx";
 
 
 
 export default function ServiceRequestOverview(){
-    const [reqs, setReqs] = useState<ServiceRequest[]>([]);
+    const [serviceRequests, setServiceRequests] = useState<ServiceRequest[]>([]);
     const [employeeList, setEmployeeList] = useState<EmployeeAutocompleteOption[]>([]);
     const reqTypes = ["MEDICINE", "SANITATION", "GIFT", "MAINTENANCE"];
+    const [filteredRequests, setFilteredRequests] = useState<ServiceRequest[]>([]);
 
     useEffect(()=>{
         axios.get('/api/service-requests').then((res: AxiosResponse) => {
-            setReqs(res.data);
+            setServiceRequests(res.data);
+            setFilteredRequests(res.data);
         });
         axios.get('/api/employees').then((res: AxiosResponse) => {
             if(res.status !== 200){
                 console.error("die");
             }
             const employeeDropdownOptions: EmployeeAutocompleteOption[] = [];
-            res.data.filter((x: AssignedEmployee) => x !== null || undefined).forEach((employee: AssignedEmployee)=> {
+            res.data.forEach((employee: AssignedEmployee)=> {
                 employeeDropdownOptions.push({
                     label: employee.firstName + " " + employee.lastName,
                     id: employee.id,
                 });
             });
-
             setEmployeeList(employeeDropdownOptions);
         });
     }, []);
 
+    function onChange(value: EmployeeAutocompleteOption){
+        console.log(value);
+        setFilteredRequests(serviceRequests.filter((request) => request.assignedEmployee?.id === value.id));
+    }
+
     return(
         <>
-            <Box
-                sx={{
-                    height: "80vh",
-                    width:'100%',
-                    overflowY:'hidden',
-                    p: 1,
-                    display:'flex',
-                    flexDirection:'row',
-                    justifyContent:'space-evenly',
-                    alignItems:'flex-start',
-                    backgroundColor: '#FFFFFF',
-                }}
-            >
-                {
-                    reqTypes.map((category)=>{
-                        return (
-                            <Box
-                                key={category}
-                                sx={{
-                                    width: ((100.0/reqTypes.length - 1)+"%"),
-                                    minHeight:'70vh',
-                                    display:'flex',
-                                    flexDirection:'column',
-                                    justifyContent:'flex-start',
-                                    alignItems:'flex-start',
-                                    p: "0.5%",
-                                    backgroundColor: '#F1F1F1',
-                                }}
-                            >
-                                <Box sx={{
-                                    display: "flex",
-                                    justifyContent: "center",
-                                    alignItem: "center",
-                                    width:'fill-available',
-                                    px: "5%",
-                                    pt: "4%",
-                                    pb: "2%",
-                                    backgroundColor: '#012d5a',
-                                }}>
-                                    <Typography
-                                        variant={"h5"}
-                                        sx={{
-                                            textAlign:'center',
-                                            color: '#f6bd38',
-                                            fontWeight: 500,
-                                        }}>{category}
-                                    </Typography>
-                                </Box>
+            <Box>
+                <Box sx={{p: 1, backgroundColor: '#FFFFFF'}}>
+                    <EmployeeAutoComplete
+                        onChange={(v: EmployeeAutocompleteOption) => {
+                            onChange(v);
+                        }}
+                        label={"Filter..."} employeeList={employeeList}></EmployeeAutoComplete>
+                </Box>
+                <Box
+                    sx={{
+                        height: "80vh",
+                        width:'100%',
+                        overflowY:'hidden',
+                        p: 1,
+                        display:'flex',
+                        flexDirection:'row',
+                        justifyContent:'space-evenly',
+                        alignItems:'flex-start',
+                    }}
+                >
+                    {
+                        reqTypes.map((category)=>{
+                            return (
                                 <Box
+                                    key={category}
                                     sx={{
+                                        width: ((100.0/reqTypes.length - 1)+"%"),
+                                        minHeight:'70vh',
+                                        display:'flex',
+                                        flexDirection:'column',
+                                        justifyContent:'flex-start',
+                                        alignItems:'flex-start',
+                                        p: "0.5%",
+                                        backgroundColor: '#F1F1F1',
+                                    }}
+                                >
+                                    <Box sx={{
                                         display: "flex",
-                                        flexDirection: "column",
-                                        width: '100%',
-                                        height: '65vh',
-                                        gap: "1rem",
-                                        overflow: 'scroll',
-                                        p: '5%',
+                                        justifyContent: "center",
+                                        alignItem: "center",
+                                        width:'fill-available',
+                                        px: "5%",
+                                        pt: "4%",
+                                        pb: "2%",
+                                        backgroundColor: '#012d5a',
                                     }}>
-                                    {
-                                        reqs.filter(x=> x.type === category).map((service) => {
-                                            return(
-                                                <KanbanBoardCard key={service.requestID} serviceRequestData={service} employeeList={employeeList} />
-                                            );
-                                        })
-                                    }
+                                        <Typography
+                                            variant={"h5"}
+                                            sx={{
+                                                textAlign:'center',
+                                                color: '#f6bd38',
+                                                fontWeight: 500,
+                                            }}>{category}
+                                        </Typography>
+                                    </Box>
+                                    <Box
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: "column",
+                                            width: '100%',
+                                            height: '65vh',
+                                            gap: "1rem",
+                                            overflow: 'scroll',
+                                            p: '5%',
+                                        }}>
+                                        {
+                                            filteredRequests.filter(x=> x.type === category).map((service) => {
+                                                return(
+                                                    <KanbanBoardCard key={service.requestID} serviceRequestData={service} employeeList={employeeList} />
+                                                );
+                                            })
+                                        }
+                                    </Box>
                                 </Box>
-                            </Box>
-                        );})
-                }
+                            );})
+                    }
+                </Box>
+                {/*<RequestInspectionDialogue*/}
+                {/*    selectedRequest={selectedCard}*/}
+                {/*    onCloseDialogue={()=>{*/}
+                {/*        setSelectedCard(null);*/}
+                {/*    }}*/}
+                {/*/>*/}
             </Box>
-            {/*<RequestInspectionDialogue*/}
-            {/*    selectedRequest={selectedCard}*/}
-            {/*    onCloseDialogue={()=>{*/}
-            {/*        setSelectedCard(null);*/}
-            {/*    }}*/}
-            {/*/>*/}
         </>
     );
 }
