7b05c484b9cc08566ec3a2bdb16718ee6b2a4f70
cacsibal <cacsibal@gmail.com>
2024-04-21 14:48:26 -0400

    fuzzy search mechanism implemented

--- apps/frontend/src/pages/SearchPage/SearchPage.tsx
+++ apps/frontend/src/pages/SearchPage/SearchPage.tsx
@@ -1,12 +1,33 @@
 import {useLocation} from "react-router-dom";
+import {search} from "../../helpers/fuzzySearch.tsx";
+
+export type PageCardInfo = {
+    path: string;
+    description: string;
+    keywords: string[];
+};
+
+const pages: PageCardInfo[] = [
+    { // Gift Request
+        path: "/gift-request",
+        description: "Purchase a gift!",
+        keywords: [
+            "purchase",
+            "buy",
+            "teddy bear",
+        ]
+    }
+];
 
 function SearchPage() {
     const location = useLocation();
     const query = location.state?.query;
 
+    const res: PageCardInfo[] = search(query, pages);
+
     return (
         <div>
-            <p>{query}</p>
+            <p>{res.map((d) => d.path)}</p>
         </div>
     );
 }
