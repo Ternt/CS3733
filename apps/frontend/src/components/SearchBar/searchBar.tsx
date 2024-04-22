905313f92095602904038d809ee52fa16d65bfe5
cacsibal <cacsibal@gmail.com>
2024-04-22 11:06:00 -0400

    updated with main

--- apps/frontend/src/components/SearchBar/searchBar.tsx
+++ apps/frontend/src/components/SearchBar/searchBar.tsx
@@ -6,56 +6,56 @@ import SearchIcon from "@mui/icons-material/Search";
 import {useNavigate} from "react-router-dom";
 
 type searchBarProps = {
-  border ?: string;
-  borderColor ?: string;
-  borderRadius ?: string;
+    border?: string;
+    borderColor?: string;
+    borderRadius?: string;
 }
 
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "#012d5a",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
+const Search = styled("div")(({theme}) => ({
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
     width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
+    [theme.breakpoints.up("sm")]: {
+        marginLeft: theme.spacing(3),
+        width: "auto",
     },
-  },
 }));
 
-function SearchBar() {
+const SearchIconWrapper = styled("div")(({theme}) => ({
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    color: "#012d5a",
+}));
+
+const StyledInputBase = styled(InputBase)(({theme}) => ({
+    color: "inherit",
+    "& .MuiInputBase-input": {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create("width"),
+        width: "100%",
+        [theme.breakpoints.up("md")]: {
+            width: "20ch",
+        },
+    },
+}));
+
+function SearchBar(props: searchBarProps) {
     const [searchQuery, setSearchQuery] = useState('');
     const navigate = useNavigate();
 
     const handleKeyDown = (event: React.KeyboardEvent) => {
         if (event.key === 'Enter') {
-            navigate('/search', { state: { query: searchQuery } });
+            navigate('/search', {state: {query: searchQuery}});
         }
     };
 
@@ -65,6 +65,11 @@ function SearchBar() {
                 height: "6vh",
                 color: "white",
                 bgcolor: "white",
+                border: (props ? props.border : "none"),
+                borderColor: (props ? props.borderColor : "none"),
+                borderRadius: (props ? props.borderRadius : "none"),
+                display: "flex",
+                alignItems: "center",
             }}
         >
             <SearchIconWrapper>
@@ -81,32 +86,6 @@ function SearchBar() {
             />
         </Search>
     );
-function SearchBar(props ?: searchBarProps) {
-  return (
-    <Search
-      sx={{
-        height: "6vh",
-        color: "white",
-        bgcolor: "white",
-        border: (props ? props.border : "none"),
-        borderColor: (props ? props.borderColor : "none"),
-        borderRadius: (props ? props.borderRadius : "none"),
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <SearchIconWrapper>
-        <SearchIcon />
-      </SearchIconWrapper>
-      <StyledInputBase
-        placeholder="Search…"
-        inputProps={{ "aria-label": "search" }}
-        sx={{
-          height: "6vh",
-        }}
-      />
-    </Search>
-  );
 }
 
 export default SearchBar;
