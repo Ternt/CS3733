7ab8bcf9d04b67ec79bd0e7a8833346cf9d90cfc
zachszeto <zacharyszto@gmail.com>
2024-04-22 12:32:05 -0400

    Implemented SMS Messaging in Backend

--- apps/backend/src/routes/sms.ts
+++ apps/backend/src/routes/sms.ts
@@ -3,9 +3,32 @@ import {Twilio} from 'twilio';
 
 const router: Router = express.Router();
 
+// Validate E164 format
+function validE164(num: string) {
+    return /^\+?[1-9]\d{1,14}$/.test(num);
+}
+
 router.post("/", async function (req: Request, res: Response) {
-    console.log(req.body);
-    //todo put the code to take the message and phone number to send from the req.body, check that its valid, and send an sms message
+    const twilioNumber = '+18666745632';
+    const accountSid = 'ACe090c19b4ac0b1699991ce7af4bd4b6d';
+    const authToken = '32a4202f264de97346ea9a9e93c3ecd8';
+
+    const client = new Twilio(accountSid, authToken);
+
+    // start sending message
+    if (!validE164('+17742908219')) {
+        throw new Error('number must be E164 format!');
+    }
+
+    client.messages
+        .create({
+            from: twilioNumber,
+            to: '+17742908219',
+            body: 'Hello',
+        })
+        .then(message => console.log(message.sid))
+        .catch(error => console.error(error));
+
     res.sendStatus(200);
 });
 
