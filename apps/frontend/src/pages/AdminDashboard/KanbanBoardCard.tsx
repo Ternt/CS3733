abd7cd7fcdffea798ef6000fd47ed474c6f27e70
Ternt <kyle.pham202@gmail.com>
2024-04-24 03:20:41 -0400

    Small API call error

--- apps/frontend/src/pages/AdminDashboard/KanbanBoardCard.tsx
+++ apps/frontend/src/pages/AdminDashboard/KanbanBoardCard.tsx
@@ -92,13 +92,15 @@ export default function KanbanBoardCard(prop: KanbanBoardProp){
             });
 
         updateEmployeeRequests(newEmployee);
-        if(statusChanged){updateAssignmentStatus("ASSIGNMENT");}
+        if(statusChanged){updateAssignmentStatus("ASSIGNED");}
     };
 
     const onChangeAssignment = (event: ChangeEvent<HTMLInputElement>) => {
         setServiceData({...serviceData, status: event.target.value});
         updateAssignmentStatus(event.target.value);
     };
+
+    console.log(prop.serviceRequestData);
 
     return(
         <Box
@@ -154,9 +156,27 @@ export default function KanbanBoardCard(prop: KanbanBoardProp){
 
                             <InformationField label={"Medicine"} data={serviceData.medicineDetail?.medicine.toLowerCase().replace("_", " ")}></InformationField>
 
+                            <InformationField label={"Requester Name"} data={serviceData.religiousDetail?.requestorName}></InformationField>
+
+                            <InformationField label={"Leader Name"} data={serviceData.religiousDetail?.religiousLeaderName}></InformationField>
+
+                            <InformationField label={"Service Type"} data={serviceData.religiousDetail?.serviceType}></InformationField>
+
+                            <InformationField label={"Date"} data={serviceData.religiousDetail?.date}></InformationField>
+
+                            <InformationField label={"Time"} data={serviceData.religiousDetail?.time}></InformationField>
+
+                            <InformationField label={"Location"} data={serviceData.religiousDetail?.endLocation}></InformationField>
+
+                            <InformationField label={"Interpreter Name"} data={serviceData.languageDetail?.name}></InformationField>
+
+                            <InformationField label={"Language"} data={serviceData.languageDetail?.language.toLowerCase()}></InformationField>
+
                             <InformationField label={"Date"} data={serviceData.sanitationDetail?.date}></InformationField>
 
                             <InformationField label={"Date"} data={serviceData.sanitationDetail?.date}></InformationField>
+
+                            <InformationField label={"Physician Name"} data={serviceData.medicineDetail?.primaryPhysicianName}></InformationField>
 
                             {(serviceData.sanitationDetail === undefined || serviceData.sanitationDetail === null)?"":(
                                 serviceData.sanitationDetail.messTypes.map((messtype)=>{
