5a0a3c5323da8bf33a9c8fe33aabc314c7910371
Ternt <kyle.pham202@gmail.com>
2024-04-27 03:42:14 -0400

    Added prop for employee list for autocompletes components. Changed up the styling to make it cleaner. Varied up the style of fields to make the form look more interesting.

--- apps/frontend/src/pages/AdminDashboard/PopoverForm.tsx
+++ apps/frontend/src/pages/AdminDashboard/PopoverForm.tsx
@@ -3,27 +3,32 @@
 
 import Box from "@mui/material/Box";
 import Modal from "@mui/material/Modal";
+// import Icon from "@mui/material/Icon";
 // import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import SubjectIcon from "@mui/icons-material/Subject";
 import InputBase from "@mui/material/InputBase";
 import {IconButton} from "@mui/material";
+import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded';
 
-import {ServiceRequest} from "../../helpers/typestuff.ts";
+// import {EmployeeAutoCompleteData, EmployeeAutoCompleteOption, ServiceRequest, AssignedEmployee} from "../../helpers/typestuff.ts";
+import {EmployeeAutoCompleteData, ServiceRequest} from "../../helpers/typestuff.ts";
+// import EmployeeAutoComplete from "../../components/EmployeeAutoComplete.tsx";
 import DataGroup from "./DataGroup.tsx";
 
 type PopoverFormProp = {
     open: boolean;
     onClose: () => void;
     data: ServiceRequest;
+    autocomplete: EmployeeAutoCompleteData;
 }
 
 export function PopoverForm(prop: PopoverFormProp){
-
-
+    // const [serviceRequest, setServiceRequest] = useState<ServiceRequest>(prop.data);
+    //
     // function updateAssignmentStatus(newStatus: string){
     //     const updatedStatus = {
-    //         requestID: serviceData.requestID,
+    //         requestID: serviceRequest.requestID,
     //         status: newStatus
     //     };
     //
@@ -47,7 +52,7 @@ export function PopoverForm(prop: PopoverFormProp){
     // function updateEmployeeRequests(newEmployee: AssignedEmployee){
     //     const newEmployeeService = {
     //         employeeID: newEmployee?.id,
-    //         requestID: serviceData.requestID
+    //         requestID: serviceRequest.requestID
     //     };
     //
     //     fetch("/api/employees/assign", {
@@ -66,10 +71,10 @@ export function PopoverForm(prop: PopoverFormProp){
     //             console.error("Error:", error);
     //         });
     // }
-
-    // const onChangeEmployee = (newValue: EmployeeAutocompleteOption) => {
+    //
+    // const onChangeEmployee = (newValue: EmployeeAutoCompleteOption) => {
     //     let statusChanged = false;
-    //     function changeStatus(employee: EmployeeAutocompleteOption, currentStatus: string): string{
+    //     function changeStatus(employee: EmployeeAutoCompleteOption, currentStatus: string): string{
     //         if(currentStatus === "UNASSIGNED"){
     //             statusChanged = true;
     //             return "ASSIGNED";
@@ -86,18 +91,19 @@ export function PopoverForm(prop: PopoverFormProp){
     //         assignedRequests: [],
     //     };
     //
-    //     setServiceData(
+    //     setServiceRequest(
     //         {
-    //             ...serviceData,
+    //             ...serviceRequest,
     //             assignedEmployee: newEmployee,
-    //             status: changeStatus(newValue, serviceData.status),
+    //             status: changeStatus(newValue, serviceRequest.status),
     //         });
     //
-    //     prop.funcUpdateSRData();
+    //     prop.autocomplete.updateFunction();
     //
     //     updateEmployeeRequests(newEmployee);
     //     if(statusChanged){updateAssignmentStatus("ASSIGNED");}
     // };
+
     //
     // const onChangeAssignment = (event: ChangeEvent<HTMLInputElement>) => {
     //     setServiceData({...serviceData, status: event.target.value});
@@ -106,9 +112,7 @@ export function PopoverForm(prop: PopoverFormProp){
 
     // the most scuffed recursion function ever
     function recurseTree<Type extends object>(label: string, data: string | number | Type, informationField: JSX.Element) {
-        if(Array.isArray(data) || label === "type"){
-            return informationField;
-        }
+        if(Array.isArray(data)){ return informationField; }
 
         if(typeof data === "object"){
             Object.entries(data)
@@ -116,57 +120,14 @@ export function PopoverForm(prop: PopoverFormProp){
                 .forEach((nestedFieldData) => {
                     return informationField = recurseTree(nestedFieldData[0], nestedFieldData[1], informationField);
                 });
-
             return(
-                <DataGroup label={label}>
-                    {informationField}
-                </DataGroup>
-            );
-        }
-
-        // if data have notes as the label, render a text field
-        if(label.toLowerCase().includes("notes")){
-            return(
-                <>
-                    <Box sx={{pb: 2}}>
-                        <Box sx={{
-                                display: 'flex',
-                                flexDirection: 'row',
-                                alignItem: 'center',
-                                width: '100%',
-                                pb: 1,
-                            }}>
-                            <IconButton sx={{p: 0}}><SubjectIcon/></IconButton>
-                            <Typography variant="h6" sx={{px: 1}}>{label}</Typography>
-                        </Box>
-                        <Box sx={{display: 'flex', flexDirection: 'row', width: '100%', padding: '0.125vh', px: '3%'}}>
-                            <Box
-                                sx={{
-                                    bgcolor: '#FFFFFF',
-                                    display: 'flex',
-                                    width: '100%',
-                                    padding: 1,
-                                    borderRadius: 2,
-                                    border: 2,
-                                    borderColor: '#E4E4E4'
-                            }}>
-                                <InputBase
-                                    fullWidth
-                                    multiline={true}
-                                    sx={{px: 1}}
-                                    placeholder={data.toString()}
-                                >
-                                </InputBase>
-                            </Box>
-                        </Box>
-                    </Box>
-                    {informationField}
-                </>
+                <DataGroup label={label}>{informationField}</DataGroup>
             );
         }
 
         return(
-            <><Box sx={{display: 'flex', flexDirection: 'row', width: '100%', padding: '0.125vh', px: '3%'}}>
+            <>
+                <Box sx={{display: 'flex', flexDirection: 'row', width: '100%', px: 3}}>
                     <Typography noWrap={false} sx={{width: '50%'}}> {label} </Typography>
                     <Typography noWrap={false} sx={{width: '50%'}}> {data.toString()}</Typography>
                 </Box>
@@ -178,22 +139,18 @@ export function PopoverForm(prop: PopoverFormProp){
     return(
         <>
             <Modal
+                sx={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}
                 keepMounted
                 open={prop.open}
                 onClose={() => {
                     prop.onClose();
-                }}
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'center',
-                    alignItems: 'center',
                 }}
             >
                 <Box sx={{
                     display: 'flex',
                     flexDirection: 'column',
                     bgcolor: '#F1F1F1',
-                    width: '40vw',
+                    width: '50vw',
                     borderRadius: 1,
                     gap: '0.5vh',
                     padding: 3,
@@ -201,23 +158,85 @@ export function PopoverForm(prop: PopoverFormProp){
                     overflowX: 'hidden',
                     height: '75vh',
                 }}>
+                    {/* title */}
+                    <Typography sx={{pb: 2, px: 4}} variant={"h4"}>
+                        Medicine Request at {prop.data.location.longName}
+                    </Typography>
 
-                    {
-                        Object.entries(prop.data)
-                            .filter((data) =>
-                                (data[1] !== null) &&
-                                (data[0] !== "requestID"))
-                            .map((data, index) => {
-                                const form = recurseTree(data[0], data[1], <></>);
-                                return(
-                                    <Box key={"field" + index} >
-                                        <Box sx={{display: 'flex', flexDirection: 'column'}}>
-                                            {form}
-                                        </Box>
-                                    </Box>
-                                );
-                            })
-                    }
+                    {/* horizontal bar displaying employee, status, and priority */}
+                    <Box sx={{display: 'flex', flexDirection: 'row', width: '100%', px: 4, pb: 1}}>
+                        <Box sx={{width: '15vw'}}>
+                            <Typography>Employee</Typography>
+                            <IconButton>
+                                <AccountCircleRoundedIcon/>
+                            </IconButton>
+                        </Box>
+                        <Box sx={{width: '15vw'}}>
+                            <Typography>Status</Typography>
+                        </Box>
+                        <Box sx={{width: '15vw'}}>
+                            <Typography>Priority</Typography>
+                        </Box>
+                    </Box>
+
+                    {/* field to display notes */}
+                    <Box sx={{pb: 2}}>
+                        <Box sx={{display: 'flex', flexDirection: 'row', alignItem: 'center', width: '100%', pb: 2,}}>
+                            <IconButton disabled={true} sx={{p: 0}}><SubjectIcon/></IconButton>
+                            <Typography sx={{px: 1}}>Notes</Typography>
+                        </Box>
+                        <Box sx={{display: 'flex', flexDirection: 'row', width: '100%', padding: '0.125vh', px: 4}}>
+                            <Box
+                                sx={{
+                                    bgcolor: '#FFFFFF',
+                                    display: 'flex',
+                                    width: '100%',
+                                    padding: 1,
+                                    borderRadius: 2,
+                                    border: 2,
+                                    borderColor: '#E4E4E4'
+                                }}>
+                                <InputBase
+                                    sx={{px: 1}}
+                                    fullWidth
+                                    multiline={true}
+                                    inputProps={{
+                                        readOnly: true,
+                                        placeholder: prop.data.notes,
+                                    }}
+                                >
+                                </InputBase>
+                            </Box>
+                        </Box>
+                    </Box>
+
+                    {/* data fields */}
+                    <Box sx={{display: 'flex', flexDirection: 'row', alignItem: 'center', width: '100%', pb: 2,}}>
+                        <IconButton disabled={true} sx={{p: 0}}><SubjectIcon/></IconButton>
+                        <Typography sx={{px: 1}}>Details</Typography>
+                    </Box>
+                   <Box sx={{display: 'flex', flexDirection: 'column', px: 4, gap: 2}}>
+                       {
+                           Object.entries(prop.data)
+                               .filter((data) =>
+                                   (data[1] !== null) &&
+                                   (data[0] !== "requestID") &&
+                                   (data[0] !== "notes") &&
+                                   (data[0] !== "priority") &&
+                                   (data[0] !== "status") &&
+                                   (data[0] !== "type"))
+                               .map((data, index) => {
+                                   const form = recurseTree(data[0], data[1], <></>);
+                                   return(
+                                       <Box key={"field" + index} sx={{border: 1, borderRadius: 1, borderColor: "#E4E4E4"}}>
+                                           <Box sx={{display: 'flex', flexDirection: 'column'}}>
+                                               {form}
+                                           </Box>
+                                       </Box>
+                                   );
+                               })
+                       }
+                   </Box>
                 </Box>
             </Modal>
         </>
