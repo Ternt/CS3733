3f70d7f948bd000f5c6c4c4cbc75e521387838d0
Ternt <kyle.pham202@gmail.com>
2024-04-28 23:00:44 -0400

    turned it into a modal popup

--- apps/frontend/src/components/DataHandling/UploadGraphData.tsx
+++ apps/frontend/src/components/DataHandling/UploadGraphData.tsx
@@ -1,70 +1,90 @@
 import React, {useState} from "react";
 import axios from "axios";
-import {Box, Button, TextField, Typography} from "@mui/material";
+
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Modal from "@mui/material/Modal";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
 
 export default function UploadGraphData(){
-  const [nodeFile, setNodeFile] = useState<File | null>(null);
-  const [edgeFile, setEdgeFile] = useState<File | null>(null);
+    const [nodeFile, setNodeFile] = useState<File | null>(null);
+    const [edgeFile, setEdgeFile] = useState<File | null>(null);
+    const [open, setOpen] = useState<boolean>(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  const handleNodeFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setNodeFile(event.target.files ? event.target.files[0] : null);
-  };
+    const handleNodeFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setNodeFile(event.target.files ? event.target.files[0] : null);
+    };
 
-  const handleEdgeFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setEdgeFile(event.target.files ? event.target.files[0] : null);
-  };
+    const handleEdgeFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setEdgeFile(event.target.files ? event.target.files[0] : null);
+    };
 
-  async function handleSubmit(event: React.FormEvent){
-    event.preventDefault();
-    if (!nodeFile || !edgeFile) return;
-    const formData = new FormData();
-    formData.append('nodes', nodeFile);
-    formData.append('edges', edgeFile);
-    console.log(formData);
-    await axios.post("/api/map/upload?header=true", formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-  }
-  return (
-    <Box
-      sx={{
-        p:5,
-        display:'flex',
-        flexDirection:'column',
-        gap:1,
-        width:'50%',
-      }}
-    >
-      <Typography variant={"h6"}>Upload new data</Typography>
-      <Typography variant={"subtitle1"}>Nodes</Typography>
-      <TextField
-        type="file"
-        InputProps={{
-          inputProps: {
-            accept: ".csv",
-          },
-        }}
-        onChange={handleNodeFileChange}
-      />
-      <Typography variant={"subtitle1"}>Edges</Typography>
-      <TextField
-        type="file"
-        InputProps={{
-          inputProps: {
-            accept: ".csv",
-          },
-        }}
-        onChange={handleEdgeFileChange}
-      />
-      <Button
-        variant="contained"
-        type="submit"
-        onClick={handleSubmit}
-      >
-        Upload
-      </Button>
-    </Box>
-  );
+    async function handleSubmit(event: React.FormEvent){
+        event.preventDefault();
+        if (!nodeFile || !edgeFile) return;
+        const formData = new FormData();
+        formData.append('nodes', nodeFile);
+        formData.append('edges', edgeFile);
+        console.log(formData);
+        await axios.post("/api/map/upload?header=true", formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+    }
+
+    return (
+        <>
+            <Box
+                sx={{
+                    display:'flex',
+                    flexDirection:'column',
+                    marginRight: 3,
+                }}
+            >
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    sx={{display: 'flex', justifyContent: 'center', alignItems: 'center',}}
+                >
+                    <Box sx={{height: '30vh', padding: 3, borderRadius: 1, bgcolor: '#FFFFFF'}}>
+                        <Typography variant={"h6"}>Upload new data</Typography>
+                        <Typography variant={"subtitle1"}>Nodes</Typography>
+                        <TextField
+                            type="file"
+                            InputProps={{
+                                inputProps: {
+                                    accept: ".csv",
+                                },
+                            }}
+                            onChange={handleNodeFileChange}
+                        />
+                        <Typography variant={"subtitle1"}>Edges</Typography>
+                        <TextField
+                            type="file"
+                            InputProps={{
+                                inputProps: {
+                                    accept: ".csv",
+                                },
+                            }}
+                            onChange={handleEdgeFileChange}
+                        />
+                        <Button
+                            variant="contained"
+                            type="submit"
+                            onClick={handleSubmit}
+                        >
+                            Submit
+                        </Button>
+                    </Box>
+                </Modal>
+                <Button variant="contained" onClick={handleOpen} sx={{boxShadow: 0}}>
+                    Upload CSV
+                </Button>
+            </Box>
+        </>
+    );
 }
