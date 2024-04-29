7a65b99cf6ea6f3ee130621cc73d5c064e237a52
Ternt <kyle.pham202@gmail.com>
2024-04-28 21:39:51 -0400

    Changed styling

--- apps/frontend/src/components/DataHandling/DataTable.tsx
+++ apps/frontend/src/components/DataHandling/DataTable.tsx
@@ -1,119 +1,127 @@
 import Toolbar from "@mui/material/Toolbar";
 import {
-  MenuItem,
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow, TextField,
-  Typography
+    MenuItem,
+    Paper,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TablePagination,
+    TableRow, TextField,
 } from "@mui/material";
 import React, {useMemo, useState} from "react";
+import Box from "@mui/material/Box";
 
 
 type DataTableProps = {
-  headers:string[];
-  rows:(string[] | string )[][];
-  title:string;
+    headers:string[];
+    rows:(string[] | string )[][];
+    title:string;
 
 };
 
 export default function DataTable(props: DataTableProps){
 
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(10);
-  const visibleRows = useMemo(() =>
-      props.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-    [page, props.rows, rowsPerPage],
-  );
+    const [page, setPage] = useState(0);
+    const [rowsPerPage, setRowsPerPage] = useState(10);
+    const visibleRows = useMemo(() =>
+            props.rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
+        [page, props.rows, rowsPerPage],
+    );
 
-  const handleChangePageE = (event: unknown, newPage: number) => {
-    setPage(newPage);
-  };
+    const handleChangePageE = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
 
-  const handleChangeRowsPerPage = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
+    const handleChangeRowsPerPage = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
 
-  return (
-    <Paper>
-      <Toolbar
-        sx={{
-          pl: { sm: 2 },
-          pr: { xs: 1, sm: 1 },
-        }}
-      >
-        <Typography
-          sx={{ flex: "1 1 100%" }}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          {props.title}
-        </Typography>
-      </Toolbar>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="Edge Table">
-          <TableHead>
-            <TableRow>
-              {props.headers.map((h)=><TableCell align="left">{h}</TableCell>)}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {visibleRows.map(
-              (row, index) => {
-                return (
-                  <TableRow
-                    key={index}
+    return (
+        <>
+            <Paper elevation={0} sx={{padding: 0}}>
+                <Toolbar variant={'dense'}
+                         sx={{
+                             color: '#E4E4E4',
+                             borderTop: 1,
+                             borderBottom: 0,
+                             borderLeft: 0,
+                             borderRight: 0,
+                             height: '8vh'
+                         }}/>
+                <TableContainer
                     sx={{
-                      "&:last-child td, &:last-child th": { border: 0 },
-                    }}
-                  >
-                    {row.map((cell)=>
-                      <TableCell align="left">{
-                        (typeof cell === "string") ? cell : (
-                          <TextField
-                            required
-                            select
-                            value={cell[0]}
-                            margin="dense"
-                            inputProps={{MenuProps: {disableScrollLock: true}}}
-                            onChange={(event) => {
-                              console.log(event.target.value);
-                            }}
-                            sx={{
-                              marginY: 0,
-                              width:'10rem'
-                            }}
-                          >
-                            {
-                              cell.map(c=><MenuItem value={c}>{c}</MenuItem>)
-                            }
-                          </TextField>
-                        )}
-                      </TableCell>
-                    )}
-                  </TableRow>
-                );
-              },
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        component="div"
-        count={props.rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePageE}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </Paper>
-  );
+                        color: '#E4E4E4',
+                        borderTop: 1,
+                        borderBottom: 1,
+                        borderLeft: 0,
+                        borderRight: 0,
+                    }}>
+                    <Table sx={{ minWidth: 650 }} aria-label="Edge Table">
+                        <TableHead>
+                            <TableRow>
+                                {props.headers.map((h)=><TableCell align="left">{h}</TableCell>)}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {visibleRows.map(
+                                (row, index) => {
+                                    return (
+                                        <TableRow
+                                            key={index}
+                                            sx={{
+                                                "&:last-child td, &:last-child th": { border: 0 },
+                                            }}
+                                        >
+                                            {row.map((cell)=>
+                                                <TableCell align="left">{
+                                                    (typeof cell === "string") ? cell : (
+                                                        <TextField
+                                                            required
+                                                            select
+                                                            value={cell[0]}
+                                                            margin="dense"
+                                                            inputProps={{MenuProps: {disableScrollLock: true}}}
+                                                            onChange={(event) => {
+                                                                console.log(event.target.value);
+                                                            }}
+                                                            sx={{
+                                                                marginY: 0,
+                                                                width:'10rem'
+                                                            }}
+                                                        >
+                                                            {
+                                                                cell.map(c=><MenuItem value={c}>{c}</MenuItem>)
+                                                            }
+                                                        </TextField>
+                                                    )}
+                                                </TableCell>
+                                            )}
+                                        </TableRow>
+                                    );
+                                },
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <Box>
+                    <TablePagination
+                        component="div"
+                        count={props.rows.length}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePageE}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Box>
+            </Paper>
+            <Box id={"buffer"} sx={{height: '2vh', width: '100%'}}></Box>
+        </>
+    );
 }
+
+
