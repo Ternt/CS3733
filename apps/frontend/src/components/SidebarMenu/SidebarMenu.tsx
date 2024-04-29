874dd47ec91840d616dc14e9ad9144d1b6ce8b8f
Ternt <kyle.pham202@gmail.com>
2024-04-28 21:39:19 -0400

    Some styling for

--- apps/frontend/src/components/SidebarMenu/SidebarMenu.tsx
+++ apps/frontend/src/components/SidebarMenu/SidebarMenu.tsx
@@ -2,12 +2,14 @@ import { ReactNode, useState } from "react";
 import { Box, Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import { motion } from "framer-motion";
+import {SxProps} from "@mui/system";
 
 type SidebarMenuProps = {
-  value: number;
-  onSelect: (index: number) => void;
-  tabs: string[];
-  children: ReactNode[];
+    sx?: SxProps;
+    value: number;
+    onSelect: (index: number) => void;
+    tabs: string[];
+    children: ReactNode[];
 };
 
 export default function SidebarMenu(props: SidebarMenuProps) {
@@ -15,60 +17,62 @@ export default function SidebarMenu(props: SidebarMenuProps) {
 
   return (
     <>
-      <Box
-        sx={{
-          width: "8vw",
-          height: "90vh",
-        }}
-      >
         <Box
-          sx={{
-            position:'fixed',
-            width:'8vw'
-          }}
+            sx={{
+                width: "8vw",
+                height: "90vh",
+            }}
         >
-          {props.tabs.map((t, i) => {
-            return (
-              <Button
-                key={i}
+            <Box
                 sx={{
-                  width: "100%",
-                  height:'5rem',
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  p: 2,
+                    ...props.sx,
+                    position:'fixed',
+                    height: '90vh',
+                    width:'8vw'
                 }}
-                onClick={() => {
-                  setSelectedTab(i);
-                  props.onSelect(i);
-                }}
-              >
-                {props.children[i]}
-                <Typography
-                  variant={"subtitle2"}
-                  sx={{
-                    width: "100%",
-                    textAlign: "center",
-                    textWrap: "wrap",
-                  }}
-                >
-                  {t}
-                </Typography>
-                {i === selectedTab ? (
-                  <motion.div style={{
-                    backgroundColor: "#00000010",
-                    width:'100%',
-                    height:'100%',
-                    position:'absolute',
-                  }} layoutId="underline" />
-                ) : null}
-              </Button>
-            );
-          })}
+            >
+                {props.tabs.map((t, i) => {
+                    return (
+                        <Button
+                            key={i}
+                            sx={{
+                                width: "100%",
+                                height:'5rem',
+                                display: "flex",
+                                flexDirection: "column",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                p: 2,
+                            }}
+                            onClick={() => {
+                                setSelectedTab(i);
+                                props.onSelect(i);
+                            }}
+                        >
+                            {props.children[i]}
+                            <Typography
+                                variant={"subtitle2"}
+                                sx={{
+                                    width: "100%",
+                                    textAlign: "center",
+                                    textWrap: "wrap",
+                                }}
+                            >
+                                {t}
+                            </Typography>
+                            {i === selectedTab ? (
+                                <motion.div style={{
+                                    backgroundColor: "#00000010",
+                                    width:'100%',
+                                    height:'100%',
+                                    position:'absolute',
+                                }} layoutId="underline" />
+                            ) : null}
+                        </Button>
+                    );
+                })}
+            </Box>
         </Box>
-      </Box>
     </>
   );
 }
