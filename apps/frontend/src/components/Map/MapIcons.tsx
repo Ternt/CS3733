2075ea632adde127f0c37772810931ef20fa22d6
RayyanSyed21 <rssyed@wpi.edu>
2024-04-29 18:45:06 -0400

    Created Patient Check In service request

--- apps/frontend/src/components/Map/MapIcons.tsx
+++ apps/frontend/src/components/Map/MapIcons.tsx
@@ -1,5 +1,21 @@
 import ATM_ICON from "../../assets/Icons/ATMIcon.svg";
-
+import BIKE_ICON from "../../assets/Icons/BikeIcon.svg";
+import CAFE_ICON from "../../assets/Icons/CafeIcon.svg";
+import CONFERENCE_ICON from "../../assets/Icons/ConferenceIcon.svg";
+import DEPARTMENT_ICON from "../../assets/Icons/DepartmentIcon.svg";
+import ELAVATOR_ICON_1 from "../../assets/Icons/ElevatorIcon1.svg";
+import ELAVATOR_ICON_2 from "../../assets/Icons/ElevatorIcon2.svg";
+import ESCALATOR_ICON from "../../assets/Icons/EscalatorIcon.svg";
+import EXIT_ICON from "../../assets/Icons/ExitIcon.svg";
+import FOOD_ICON from "../../assets/Icons/FoodIcon.svg";
+import GIFTSHOP_ICON from "../../assets/Icons/GiftShopIcon.svg";
+import HANDICAP_ICON from "../../assets/Icons/HandicapIcon.svg";
+import INFO_ICON from "../../assets/Icons/InfoIcon.svg";
+import LAB_ICON from "../../assets/Icons/LabIcon.svg";
+import RESTROOM_ICON from "../../assets/Icons/RestroomIcon.svg";
+import SERVICE_ICON from "../../assets/Icons/ServiceIcon.svg";
+import VALET_ICON from "../../assets/Icons/ValetIcon.svg";
+import VENDING_ICON from "../../assets/Icons/VendingIcon.svg";
 
 
 export const ICONS = [
@@ -9,5 +25,13 @@ export const ICONS = [
     points: [
       {x:1444,y:1444, z:1}
     ]
-  }
+  },
+    {
+        name: "BIKE",
+        icon: BIKE_ICON,
+        points: [
+            {x: , y: , z: }
+        ]
+    },
+
 ];
