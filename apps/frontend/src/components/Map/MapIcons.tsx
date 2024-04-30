eb38ac8e6503d589fb9473a0bebcac1eed7a02d5
DecentSalad <bgerlach43@yahoo.com>
2024-04-29 22:31:27 -0400

    first half icons

--- apps/frontend/src/components/Map/MapIcons.tsx
+++ apps/frontend/src/components/Map/MapIcons.tsx
@@ -1,13 +1,13 @@
 // Brett
-// import ATM_ICON from "../../assets/Icons/ATMIcon.svg";
-// import BIKE_ICON from "../../assets/Icons/BikeIcon.svg";
-// import CAFE_ICON from "../../assets/Icons/CafeIcon.svg";
-// import CONFERENCE_ICON from "../../assets/Icons/ConferenceIcon.svg";
-// import DEPARTMENT_ICON from "../../assets/Icons/DepartmentIcon.svg";
-// import ELEVATOR_ICON_1 from "../../assets/Icons/ElevatorIcon1.svg";
-// import ELEVATOR_ICON_2 from "../../assets/Icons/ElevatorIcon2.svg";
-// import ESCALATOR_ICON from "../../assets/Icons/EscalatorIcon.svg";
-// import EXIT_ICON from "../../assets/Icons/ExitIcon.svg";
+import ATM_ICON from "../../assets/Icons/ATMIcon.svg";
+import BIKE_ICON from "../../assets/Icons/BikeIcon.svg";
+import CAFE_ICON from "../../assets/Icons/CafeIcon.svg";
+import CONFERENCE_ICON from "../../assets/Icons/ConferenceIcon.svg";
+import DEPARTMENT_ICON from "../../assets/Icons/DepartmentIcon.svg";
+import ELEVATOR_ICON_1 from "../../assets/Icons/ElevatorIcon1.svg";
+import ELEVATOR_ICON_2 from "../../assets/Icons/ElevatorIcon2.svg";
+import ESCALATOR_ICON from "../../assets/Icons/EscalatorIcon.svg";
+import EXIT_ICON from "../../assets/Icons/ExitIcon.svg";
 
 // Anton
 // import FOOD_ICON from "../../assets/Icons/FoodIcon.svg";
@@ -29,5 +29,199 @@
 // },
 
 export const ICONS = [
+    {
+     name: "ATM",
+         icon: ATM_ICON,
+     points: [
+         {x:1562, y:1877, z:2},
+         {x:1928, y:856, z:2},
+ ]
+ },
+    {
+        name: "BIKE",
+        icon: BIKE_ICON,
+        points: [
+            {x:1433, y:2881, z:0},
+        ]
+    },
+    {
+        name: "CAFE",
+        icon: CAFE_ICON,
+        points: [
+            {x:1457, y:1228, z:2},
+            {x:3289, y:986, z:3},
+            {x:4214, y:513, z:3},
+        ]
+    },
+    {
+        name: "CONFERENCE",
+        icon: CONFERENCE_ICON,
+        points: [
+
+        ]
+    },
+    {
+        name: "DEPARTMENT",
+        icon: DEPARTMENT_ICON,
+        points: [
+
+        ]
+    },
+    {
+        name: "ELEVATOR_ICON_1",
+        icon: ELEVATOR_ICON_1,
+        points: [
+            {x:1616, y:2726, z:0},
+            {x:1606, y:2103, z:0},
+            {x:1808, y:1298, z:0},
+            {x:1775, y:944, z:0},
+            {x:1773, y:884, z:0},
+            {x:2174, y:969, z:0},
+            {x:2313, y:792, z:0},
+            {x:2381, y:790, z:0},
+            {x:1607, y:2102, z:1},
+            {x:1805, y:1297, z:1},
+            {x:1769, y:941, z:1},
+            {x:1777, y:879, z:1},
+            {x:2177, y:963, z:1},
+            {x:2310, y:786, z:1},
+            {x:2378, y:797, z:1},
+            {x:2704, y:1083, z:1},
+            {x:1517, y:2761, z:2},
+            {x:1637, y:2112, z:2},
+            {x:1634, y:1951, z:2},
+            {x:1631, y:1881, z:2},
+            {x:1797, y:1305, z:2},
+            {x:1797, y:951, z:2},
+            {x:1794, y:886, z:2},
+            {x:2340, y:804, z:2},
+            {x:2411, y:799, z:2},
+            {x:2202, y:973, z:2},
+            {x:2724, y:1093, z:2},
+            {x:2925, y:867, z:2},
+            {x:3050, y:946, z:2},
+            {x:1519, y:2769, z:3},
+            {x:1617, y:2113, z:3},
+            {x:1614, y:1961, z:3},
+            {x:1620, y:1901, z:3},
+            {x:1820, y:1311, z:3},
+            {x:1795, y:946, z:3},
+            {x:1795, y:873, z:3},
+            {x:2197, y:962, z:3},
+            {x:2356, y:797, z:3},
+            {x:2422, y:804, z:3},
+            {x:2717, y:1095, z:3},
+            {x:2914, y:870, z:3},
+            {x:3066, y:927, z:3},
+            {x:3228, y:854, z:3},
+            {x:3456, y:759, z:3},
+            {x:3935, y:880, z:3},
+            {x:4144, y:639, z:3},
+            {x:4563, y:347, z:3},
+            {x:1538, y:2769, z:4},
+            {x:1622, y:2093, z:4},
+            {x:1620, y:1939, z:4},
+            {x:1617, y:1867, z:4},
+            {x:1809, y:1321, z:4},
+            {x:1774, y:957, z:4},
+            {x:1779, y:895, z:4},
+            {x:2175, y:985, z:4},
+            {x:2325, y:803, z:4},
+            {x:2397, y:803, z:4},
+            {x:2706, y:1102, z:4},
+            {x:2905, y:880, z:4},
+            {x:3204, y:868, z:4},
+            {x:3453, y:771, z:4},
+            {x:3924, y:890, z:4},
+        ]
+    },
+    {
+        name: "ELEVATOR_ICON_2",
+        icon: ELEVATOR_ICON_2,
+        points: [
+            {x:1261, y:2734, z:0},
+            {x:1275, y:2669, z:0},
+            {x:1506, y:2022, z:0},
+            {x:1248, y:2022, z:0},
+            {x:1536, y:1610, z:0},
+            {x:1242, y:1595, z:0},
+            {x:1155, y:1266, z:0},
+            {x:1352, y:878, z:0},
+            {x:1556, y:878, z:0},
+            {x:1549, y:955, z:0},
+            {x:1683, y:985, z:0},
+            {x:2194, y:1319, z:0},
+            {x:2471, y:795, z:0},
+            {x:2605, y:815, z:0},
+            {x:1511, y:2016, z:1},
+            {x:1254, y:2016, z:1},
+            {x:1164, y:1262, z:1},
+            {x:1344, y:865, z:1},
+            {x:1560, y:893, z:1},
+            {x:1543, y:960, z:1},
+            {x:1680, y:980, z:1},
+            {x:2191, y:1331, z:1},
+            {x:2460, y:801, z:1},
+            {x:2617, y:818, z:1},
+            {x:3539, y:554, z:1},
+            {x:4149, y:649, z:1},
+            {x:4553, y:345, z:1},
+            {x:1565, y:2669, z:2},
+            {x:1515, y:2674, z:2},
+            {x:1117, y:2781, z:2},
+            {x:1122, y:2689, z:2},
+            {x:1535, y:2025, z:2},
+            {x:1282, y:2030, z:2},
+            {x:1575, y:963, z:2},
+            {x:1573, y:890, z:2},
+            {x:2492, y:811, z:2},
+            {x:2635, y:831, z:2},
+            {x:1559, y:2678, z:3},
+            {x:1502, y:2681, z:3},
+            {x:1124, y:2773, z:3},
+            {x:1129, y:2683, z:3},
+            {x:1521, y:2031, z:3},
+            {x:1262, y:2018, z:3},
+            {x:1569, y:955, z:3},
+            {x:1553, y:876, z:3},
+            {x:2488, y:805, z:3},
+            {x:1542, y:2666, z:4},
+            {x:1477, y:2673, z:4},
+            {x:1111, y:2680, z:4},
+            {x:1096, y:2770, z:4},
+            {x:1517, y:2005, z:4},
+            {x:1560, y:964, z:4},
+            {x:1554, y:894, z:4},
+            {x:2473, y:808, z:4},
+            {x:3486, y:1415, z:4},
+            {x:3492, y:1168, z:4},
+            {x:4145, y:647, z:4},
+            {x:3902, y:1089, z:4},
+            {x:4344, y:615, z:4},
+            {x:4552, y:348, z:4},
+            {x:4663, y:987, z:4},
+        ]
+    },
+    {
+        name: "ESCALATOR_ICON",
+        icon: ESCALATOR_ICON,
+        points: [
+            {x:1703, y:2088, z:1},
+            {x:1733, y:2074, z:2},
+            {x:1684, y:1905, z:2},
+            {x:1910, y:1192, z:2},
+            {x:1668, y:1975, z:3},
+            {x:1671, y:1860, z:3},
+            {x:1885, y:1216, z:3},
+            {x:1667, y:1939, z:4},
+        ]
+    },
+    {
+        name: "EXIT_ICON",
+        icon: EXIT_ICON,
+        points: [
+
+        ]
+    },
 
 ];
