64280122dc298ed0bca69a794fd80e259de19632
cacsibal <cacsibal@gmail.com>
2024-04-29 21:56:11 -0400

    icons linked

--- apps/frontend/src/components/Map/MapIcons.tsx
+++ apps/frontend/src/components/Map/MapIcons.tsx
@@ -10,15 +10,15 @@
 // import EXIT_ICON from "../../assets/Icons/ExitIcon.svg";
 
 // Anton
-// import FOOD_ICON from "../../assets/Icons/FoodIcon.svg";
-// import GIFTSHOP_ICON from "../../assets/Icons/GiftShopIcon.svg";
-// import HANDICAP_ICON from "../../assets/Icons/HandicapIcon.svg";
-// import INFO_ICON from "../../assets/Icons/InfoIcon.svg";
-// import LAB_ICON from "../../assets/Icons/LabIcon.svg";
-// import RESTROOM_ICON from "../../assets/Icons/RestroomIcon.svg";
-// import SERVICE_ICON from "../../assets/Icons/ServiceIcon.svg";
-// import VALET_ICON from "../../assets/Icons/ValetIcon.svg";
-// import VENDING_ICON from "../../assets/Icons/VendingIcon.svg";
+import FOOD_ICON from "../../assets/Icons/FoodIcon.svg";
+import GIFTSHOP_ICON from "../../assets/Icons/GiftShopIcon.svg";
+import HANDICAP_ICON from "../../assets/Icons/HandicapIcon.svg";
+import INFO_ICON from "../../assets/Icons/InfoIcon.svg";
+import LAB_ICON from "../../assets/Icons/LabIcon.svg";
+import RESTROOM_ICON from "../../assets/Icons/RestroomIcon.svg";
+import SERVICE_ICON from "../../assets/Icons/ServiceIcon.svg";
+import VALET_ICON from "../../assets/Icons/ValetIcon.svg";
+import VENDING_ICON from "../../assets/Icons/VendingIcon.svg";
 
 // {
 //     name: "BIKE",
@@ -30,4 +30,96 @@
 
 export const ICONS = [
 
+    {
+        name: "FOOD",
+        icon: FOOD_ICON,
+        points: [
+            {x:1813, y:811, z:3},
+            {x:1600, y:2501, z:2},
+        ]
+    },
+
+    {
+        name: "GIFTSHOP",
+        icon: GIFTSHOP_ICON,
+        points: [
+            {x:2019, y:1163, z:3},
+        ]
+    },
+
+    {
+        name: "HANDICAP",
+        icon: HANDICAP_ICON,
+        points: [
+            {x:3110, y:1102, z:3},
+            {x:2049, y:742, z:2},
+            {x:2045, y:1367, z:2},
+            {x:1728, y:1369, z:2},
+            {x:1734, y:1708, z:2},
+        ]
+    },
+
+    {
+        name: "INFO",
+        icon: INFO_ICON,
+        points: [
+            {x:4185, y:603, z:3},
+            {x:2970, y:921, z:3},
+            {x:1611, y:1812, z:3},
+            {x:1668, y:1193, z:2},
+        ]
+    },
+
+    {
+        name: "LAB",
+        icon: LAB_ICON,
+        points: [
+
+        ]
+    },
+
+    {
+        name: "RESTROOM",
+        icon: RESTROOM_ICON,
+        points: [
+            {x:2724, y:1134, z:4},
+            {x:2555, y:1230, z:4},
+            {x:1760, y:982, z:4},
+            {x:4110, y:592, z:3},
+            {x:3868, y:688, z:3},
+            {x:2733, y:1134, z:3},
+            {x:2570, y:1366, z:3},
+            {x:1270, y:1839, z:3},
+            {x:2899, y:719, z:2},
+            {x:2736, y:1179, z:2},
+            {x:2719, y:1140, z:1},
+            {x:2053, y:1227, z:1},
+        ]
+    },
+
+    {
+        name: "SERVICE",
+        icon: SERVICE_ICON,
+        points: [
+
+        ]
+    },
+
+    {
+        name: "VALET",
+        icon: VALET_ICON,
+        points: [
+            {x:1578, y:1303, z:2},
+            {x:1464, y:1408, z:2},
+            {x:1669, y:2728, z:0},
+        ]
+    },
+
+    {
+        name: "VENDING",
+        icon: VENDING_ICON,
+        points: [
+
+        ]
+    },
 ];
